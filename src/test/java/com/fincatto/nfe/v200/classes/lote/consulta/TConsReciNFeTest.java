f48944618ae09e26444e6d8fdc998ccb2e0e6086
Diego Fincatto <diego@fincatto.com>
2013-07-31 18:55:05 -0300

    Comentado codigo que fazia saida de retorno pro console.

--- src/test/java/com/fincatto/nfe/v200/classes/lote/consulta/TConsReciNFeTest.java
+++ src/test/java/com/fincatto/nfe/v200/classes/lote/consulta/TConsReciNFeTest.java
@@ -8,7 +8,6 @@ import org.simpleframework.xml.core.Persister;
 import org.simpleframework.xml.stream.Format;
 
 import com.fincatto.nfe.transformers.NFRegistryMatcher;
-import com.fincatto.nfe.v200.classes.lote.consulta.TConsReciNFe;
 
 public class TConsReciNFeTest {
 	
@@ -18,8 +17,8 @@ public class TConsReciNFeTest {
 		TConsReciNFe consultaLoteLido = persister.read(TConsReciNFe.class, TConsReciNFeTest.class.getResourceAsStream("lote_consulta.xml"));
 		try (StringWriter consultaLoteGerado = new StringWriter()) {
 			persister.write(consultaLoteLido, consultaLoteGerado);
-			System.out.println(consultaLoteLido.toString());
-			System.out.println(consultaLoteGerado.toString());
+			// System.out.println(consultaLoteLido.toString());
+			// System.out.println(consultaLoteGerado.toString());
 			Assert.assertEquals(consultaLoteLido.toString(), consultaLoteGerado.toString());
 		}
 	}
