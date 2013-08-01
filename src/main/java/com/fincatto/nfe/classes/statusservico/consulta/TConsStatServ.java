124272594dc7acf291f406e608a476af842571f7
Diego Fincatto <diego@fincatto.com>
2013-08-01 14:52:28 -0300

    Criado classes e webservice de consulta de status do servico.

---
+++ src/main/java/com/fincatto/nfe/classes/statusservico/consulta/TConsStatServ.java
@@ -0,0 +1,66 @@
+package com.fincatto.nfe.classes.statusservico.consulta;
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Namespace;
+import org.simpleframework.xml.Root;
+
+import com.fincatto.nfe.NFAmbiente;
+import com.fincatto.nfe.NFUnidadeFederativa;
+import com.fincatto.nfe.classes.NFe;
+
+@Root(name = "consStatServ")
+@Namespace(reference = "http://www.portalfiscal.inf.br/nfe")
+public class TConsStatServ extends NFe {
+	
+	@Attribute(name = "versao", required = true)
+	private String versao;
+	
+	@Element(name = "tpAmb", required = true)
+	private NFAmbiente ambiente;
+	
+	@Element(name = "cUF", required = true)
+	private NFUnidadeFederativa uf;
+	
+	@Element(name = "xServ", required = true)
+	private String servico;
+	
+	public TConsStatServ() {
+		this.versao = null;
+		this.ambiente = null;
+		this.uf = null;
+		this.servico = null;
+	}
+	
+	public String getVersao() {
+		return this.versao;
+	}
+	
+	public void setVersao(final String versao) {
+		this.versao = versao;
+	}
+	
+	public NFAmbiente getAmbiente() {
+		return this.ambiente;
+	}
+	
+	public void setAmbiente(final NFAmbiente ambiente) {
+		this.ambiente = ambiente;
+	}
+	
+	public NFUnidadeFederativa getUf() {
+		return this.uf;
+	}
+	
+	public void setUf(final NFUnidadeFederativa uf) {
+		this.uf = uf;
+	}
+	
+	public String getServico() {
+		return this.servico;
+	}
+	
+	public void setServico(final String servico) {
+		this.servico = servico;
+	}
+}
