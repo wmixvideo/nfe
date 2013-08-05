8cb08347ccfb8515cca572cfcbac2b8e2953350d
Diego Fincatto <diego@fincatto.com>
2013-08-05 10:54:30 -0300

    Adicionado metodos acessorios na primeira classe do icms

--- src/main/java/com/fincatto/nfe/classes/nota/NFNotaInfoItemImpostoICMS00.java
+++ src/main/java/com/fincatto/nfe/classes/nota/NFNotaInfoItemImpostoICMS00.java
@@ -27,4 +27,60 @@ public class NFNotaInfoItemImpostoICMS00 extends NFBase {
 	@Element(name = "vICMS", required = true)
 	private BigDecimal valorTributo;
 	
+	public NFNotaInfoItemImpostoICMS00() {
+		this.origem = null;
+		this.codigoSituacaoTributaria = null;
+		this.modalidadeBaseCalculo = null;
+		this.valorBaseCalculo = null;
+		this.aliquota = null;
+		this.valorTributo = null;
+	}
+	
+	public NFOrigem getOrigem() {
+		return this.origem;
+	}
+	
+	public void setOrigem(final NFOrigem origem) {
+		this.origem = origem;
+	}
+	
+	public String getCodigoSituacaoTributaria() {
+		return this.codigoSituacaoTributaria;
+	}
+	
+	public void setCodigoSituacaoTributaria(final String codigoSituacaoTributaria) {
+		this.codigoSituacaoTributaria = codigoSituacaoTributaria;
+	}
+	
+	public NFNotaInfoItemImpostoICMSModalidadeBaseCalulo getModalidadeBaseCalculo() {
+		return this.modalidadeBaseCalculo;
+	}
+	
+	public void setModalidadeBaseCalculo(final NFNotaInfoItemImpostoICMSModalidadeBaseCalulo modalidadeBaseCalculo) {
+		this.modalidadeBaseCalculo = modalidadeBaseCalculo;
+	}
+	
+	public BigDecimal getValorBaseCalculo() {
+		return this.valorBaseCalculo;
+	}
+	
+	public void setValorBaseCalculo(final BigDecimal valorBaseCalculo) {
+		this.valorBaseCalculo = valorBaseCalculo;
+	}
+	
+	public BigDecimal getAliquota() {
+		return this.aliquota;
+	}
+	
+	public void setAliquota(final BigDecimal aliquota) {
+		this.aliquota = aliquota;
+	}
+	
+	public BigDecimal getValorTributo() {
+		return this.valorTributo;
+	}
+	
+	public void setValorTributo(final BigDecimal valorTributo) {
+		this.valorTributo = valorTributo;
+	}
 }
