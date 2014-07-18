8699b9f7cb660e0b6baf25b07e98acab2d1d8e0a
Jeferson Viana Perito <jefperito@gmail.com>
2014-07-18 18:25:44 -0300

    Classes mapeadas

--- src/main/java/com/fincatto/nfe/classes/nota/NFNotaInfoItemImpostoCOFINS.java
+++ src/main/java/com/fincatto/nfe/classes/nota/NFNotaInfoItemImpostoCOFINS.java
@@ -5,55 +5,55 @@ import org.simpleframework.xml.Element;
 import com.fincatto.nfe.classes.NFBase;
 
 public class NFNotaInfoItemImpostoCOFINS extends NFBase {
-	
-	@Element(name = "COFINSAliq", required = false)
-	private NFNotaInfoItemImpostoCOFINSAliquota aliquota;
-	
-	@Element(name = "COFINSQtde", required = false)
-	private NFNotaInfoItemImpostoCOFINSQuantidade quantidade;
-	
-	@Element(name = "COFINSNT", required = false)
-	private NFNotaInfoItemImpostoCOFINSNaoTributavel naoTributavel;
-	
-	@Element(name = "COFINSOutr", required = false)
-	private NFNotaInfoItemImpostoCOFINSOutrasOperacoes outrasOperacoes;
-	
-	public NFNotaInfoItemImpostoCOFINS() {
-		this.aliquota = null;
-		this.quantidade = null;
-		this.naoTributavel = null;
-		this.outrasOperacoes = null;
-	}
-	
-	public NFNotaInfoItemImpostoCOFINSAliquota getAliquota() {
-		return this.aliquota;
-	}
-	
-	public void setAliquota(final NFNotaInfoItemImpostoCOFINSAliquota aliquota) {
-		this.aliquota = aliquota;
-	}
-	
-	public NFNotaInfoItemImpostoCOFINSQuantidade getQuantidade() {
-		return this.quantidade;
-	}
-	
-	public void setQuantidade(final NFNotaInfoItemImpostoCOFINSQuantidade quantidade) {
-		this.quantidade = quantidade;
-	}
-	
-	public NFNotaInfoItemImpostoCOFINSNaoTributavel getNaoTributavel() {
-		return this.naoTributavel;
-	}
-	
-	public void setNaoTributavel(final NFNotaInfoItemImpostoCOFINSNaoTributavel naoTributavel) {
-		this.naoTributavel = naoTributavel;
-	}
-	
-	public NFNotaInfoItemImpostoCOFINSOutrasOperacoes getOutrasOperacoes() {
-		return this.outrasOperacoes;
-	}
-	
-	public void setOutrasOperacoes(final NFNotaInfoItemImpostoCOFINSOutrasOperacoes outrasOperacoes) {
-		this.outrasOperacoes = outrasOperacoes;
-	}
+
+    @Element(name = "COFINSAliq", required = false)
+    private NFNotaInfoItemImpostoCOFINSAliquota aliquota;
+
+    @Element(name = "COFINSQtde", required = false)
+    private NFNotaInfoItemImpostoCOFINSQuantidade quantidade;
+
+    @Element(name = "COFINSNT", required = false)
+    private NFNotaInfoItemImpostoCOFINSNaoTributavel naoTributavel;
+
+    @Element(name = "COFINSOutr", required = false)
+    private NFNotaInfoItemImpostoCOFINSOutrasOperacoes outrasOperacoes;
+
+    public NFNotaInfoItemImpostoCOFINS() {
+        this.aliquota = null;
+        this.quantidade = null;
+        this.naoTributavel = null;
+        this.outrasOperacoes = null;
+    }
+
+    public NFNotaInfoItemImpostoCOFINSAliquota getAliquota() {
+        return this.aliquota;
+    }
+
+    public void setAliquota(final NFNotaInfoItemImpostoCOFINSAliquota aliquota) {
+        this.aliquota = aliquota;
+    }
+
+    public NFNotaInfoItemImpostoCOFINSQuantidade getQuantidade() {
+        return this.quantidade;
+    }
+
+    public void setQuantidade(final NFNotaInfoItemImpostoCOFINSQuantidade quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public NFNotaInfoItemImpostoCOFINSNaoTributavel getNaoTributavel() {
+        return this.naoTributavel;
+    }
+
+    public void setNaoTributavel(final NFNotaInfoItemImpostoCOFINSNaoTributavel naoTributavel) {
+        this.naoTributavel = naoTributavel;
+    }
+
+    public NFNotaInfoItemImpostoCOFINSOutrasOperacoes getOutrasOperacoes() {
+        return this.outrasOperacoes;
+    }
+
+    public void setOutrasOperacoes(final NFNotaInfoItemImpostoCOFINSOutrasOperacoes outrasOperacoes) {
+        this.outrasOperacoes = outrasOperacoes;
+    }
 }
--- src/main/java/com/fincatto/nfe/classes/nota/NFNotaInfoItemImpostoICMS60.java
+++ src/main/java/com/fincatto/nfe/classes/nota/NFNotaInfoItemImpostoICMS60.java
@@ -1,5 +1,53 @@
 package com.fincatto.nfe.classes.nota;
 
+import java.math.BigDecimal;
+
+import org.simpleframework.xml.Element;
+
+import com.fincatto.nfe.classes.NFOrigem;
+
 public class NFNotaInfoItemImpostoICMS60 {
-	
+    @Element(name = "orig", required = true)
+    private NFOrigem origem;
+
+    @Element(name = "CST", required = true)
+    private String codigoSituacaoTributaria;
+
+    @Element(name = "vBCSTRet", required = false)
+    private BigDecimal valorBCICMSSTRetido;
+
+    @Element(name = "vICMSSTRet", required = false)
+    private BigDecimal valorICMSSTRetido;
+
+    public NFOrigem getOrigem() {
+        return this.origem;
+    }
+
+    public void setOrigem(final NFOrigem origem) {
+        this.origem = origem;
+    }
+
+    public String getCodigoSituacaoTributaria() {
+        return this.codigoSituacaoTributaria;
+    }
+
+    public void setCodigoSituacaoTributaria(final String codigoSituacaoTributaria) {
+        this.codigoSituacaoTributaria = codigoSituacaoTributaria;
+    }
+
+    public BigDecimal getValorBCICMSSTRetido() {
+        return this.valorBCICMSSTRetido;
+    }
+
+    public void setValorBCICMSSTRetido(final BigDecimal valorBCICMSSTRetido) {
+        this.valorBCICMSSTRetido = valorBCICMSSTRetido;
+    }
+
+    public BigDecimal getValorICMSSTRetido() {
+        return this.valorICMSSTRetido;
+    }
+
+    public void setValorICMSSTRetido(final BigDecimal valorICMSSTRetido) {
+        this.valorICMSSTRetido = valorICMSSTRetido;
+    }
 }
