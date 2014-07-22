4309596d0f74538c89ea2a5dc19719a0c49b26df
Jeferson Viana Perito <jefperito@gmail.com>
2014-07-22 09:25:39 -0300

    Criado validacoes de string e interface de acesso

--- src/test/java/com/fincatto/nfe/validadores/StringValidatorTest.java
+++ src/test/java/com/fincatto/nfe/validadores/StringValidatorTest.java
@@ -2,6 +2,7 @@ package com.fincatto.nfe.validadores;
 
 import java.text.ParseException;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class StringValidatorTest {
@@ -19,5 +20,39 @@ public class StringValidatorTest {
     @Test(expected = ParseException.class)
     public void deveLancarExcecaoCasoEstejaSemCaracterSeparador() throws ParseException {
         StringValidador.mmaaaa("201412");
+    }
+
+    @Test
+    public void deveValidarTamanhoDeStringComRangeDe60Caracteres() {
+        StringValidador.tamanho60("oO3xPZlrBVtl4YcGaFLDmKouamLIoyIptA1fZ8CBfZWjzsjMW7ozuF1bkQrh");
+        StringValidador.tamanho60("o");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void deveLancarExcecaoCasoStringExtrapole60CaracteresOuSejaVazia() {
+        try {
+            StringValidador.tamanho60("oO3xPZlrBVtl4YcGaFLDmKouamLIoyIptA1fZ8CBfZWjzsjMW7ozuF1bkQrh1");
+            Assert.fail("Validacao falhou");
+        } catch (final IllegalStateException e) {
+            StringValidador.tamanho60("");
+        }
+        Assert.fail("Validacao falhou");
+    }
+
+    @Test
+    public void deveValidarTamanhoDeStringComRangeDe22Caracteres() {
+        StringValidador.tamanho22("AZgCErXz4DlSwRmsJaeyUV");
+        StringValidador.tamanho22("o");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void deveLancarExcecaoCasoStringExtrapole22CaracteresOuSejaVazia() {
+        try {
+            StringValidador.tamanho22("AZgCErXz4DlSwRmsJaeyUV1");
+            Assert.fail("Validacao falhou");
+        } catch (final IllegalStateException e) {
+            StringValidador.tamanho22("");
+        }
+        Assert.fail("Validacao falhou");
     }
 }
