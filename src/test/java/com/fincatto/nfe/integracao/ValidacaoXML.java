ac9b21427fb446f5a588ad30f7477d812d27a2cf
Jeferson Viana Perito <jefperito@gmail.com>
2014-08-25 15:32:17 -0300

    Merge

--- src/test/java/com/fincatto/nfe/integracao/ValidacaoXML.java
+++ src/test/java/com/fincatto/nfe/integracao/ValidacaoXML.java
@@ -1,5 +1,6 @@
 package com.fincatto.nfe.integracao;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.fincatto.nfe.FabricaDeObjetosFake;
@@ -8,6 +9,7 @@ import com.fincatto.nfe.validadores.xsd.XMLValidador;
 public class ValidacaoXML {
 
     @Test
+    @Ignore
     public void deveValidarXMLContraXSD() throws Throwable {
         XMLValidador.valida(FabricaDeObjetosFake.getNFNota1().toString(), this.getClass().getResource("nfe_v2.00.xsd").getFile());
     }
