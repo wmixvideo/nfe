12d06422c0e0d4d1cd45dac3e47873e178d68f65
Jeferson Viana Perito <jefperito@gmail.com>
2014-08-26 13:19:01 -0300

    Nota Tecnica 2012/05

--- src/test/java/com/fincatto/nfe/classes/NFOrigemTest.java
+++ src/test/java/com/fincatto/nfe/classes/NFOrigemTest.java
@@ -10,5 +10,10 @@ public class NFOrigemTest {
         Assert.assertEquals("0", NFOrigem.NACIONAL.getCodigo());
         Assert.assertEquals("1", NFOrigem.ESTRANGEIRA_IMPORTACAO_DIRETA.getCodigo());
         Assert.assertEquals("2", NFOrigem.ESTRANGEIRA_ADQUIRIDA_MERCADO_INTERNO.getCodigo());
+        Assert.assertEquals("3", NFOrigem.NACIONAL_MERCADORIA_OU_BEM_CONTEUDO_IMPORTACAO_SUPERIOR_40_P.getCodigo());
+        Assert.assertEquals("4", NFOrigem.NACIONAL_PRODUCAO_EM_CONFORMIDADE_COM_PROCESSOS_PRODUTIVOS_BASICOS.getCodigo());
+        Assert.assertEquals("5", NFOrigem.NACIONAL_MERCADORIA_OU_BEM_CONTEUDO_IMPORTACAO_INFERIOR_40_P.getCodigo());
+        Assert.assertEquals("6", NFOrigem.ESTRANGEIRA_IMPORTACAO_DIRETA_SEM_SIMILAR_NACIONAL_CONSTANTE_EM_LISTA_CAMEX.getCodigo());
+        Assert.assertEquals("7", NFOrigem.ESTRANGEIRA_ADQUIRIDA_MERCADO_INTERNO_SEM_SIMILAR_NACIONAL_CONSTANTE_EM_LISTA_CAMEX.getCodigo());
     }
 }
