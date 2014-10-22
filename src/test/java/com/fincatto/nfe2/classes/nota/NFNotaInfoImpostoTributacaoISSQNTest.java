60805c17268f261360640d2673102b879154e82a
Jeferson Viana Perito <jefperito@gmail.com>
2014-10-22 15:49:00 -0200

    adicionado testes v2

---
+++ src/test/java/com/fincatto/nfe2/classes/nota/NFNotaInfoImpostoTributacaoISSQNTest.java
@@ -0,0 +1,19 @@
+package com.fincatto.nfe2.classes.nota;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.fincatto.nfe2.classes.NFNotaInfoImpostoTributacaoISSQN;
+
+public class NFNotaInfoImpostoTributacaoISSQNTest {
+
+    @Test
+    public void deveRetornarNuloAoPassarCodigoInvalido() {
+        Assert.assertNull(NFNotaInfoImpostoTributacaoISSQN.valueOfCodigo(""));
+    }
+
+    @Test
+    public void deveRetornarTributacaoISSQNaoPassarCodigoValido() {
+        Assert.assertNotNull(NFNotaInfoImpostoTributacaoISSQN.valueOfCodigo(NFNotaInfoImpostoTributacaoISSQN.SUBSTITUTA.getCodigo()));
+    }
+}
