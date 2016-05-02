31a9ef207cff17832b2a88dd5367dcd1fc0ee9ea
Diego Fincatto <diego@fincatto.com>
2016-05-02 17:56:25 -0300

    Removido import nao utilizado.

--- src/main/java/com/fincatto/nfe310/webservices/WSLoteEnvio.java
+++ src/main/java/com/fincatto/nfe310/webservices/WSLoteEnvio.java
@@ -9,7 +9,6 @@ import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.util.AXIOMUtil;
 import org.apache.axis2.AxisFault;
 import org.apache.log4j.Logger;
-import org.simpleframework.xml.core.Persister;
 
 import com.fincatto.nfe310.NFeConfig;
 import com.fincatto.nfe310.assinatura.AssinaturaDigital;
@@ -33,104 +32,104 @@ import com.fincatto.nfe310.webservices.gerado.NfeAutorizacaoStub.NfeDadosMsg;
 
 class WSLoteEnvio {
 
-    private static final String NFE_ELEMENTO = "NFe";
-    private static final Logger LOG = Logger.getLogger(WSLoteEnvio.class);
-    private final NFeConfig config;
+	private static final String NFE_ELEMENTO = "NFe";
+	private static final Logger LOG = Logger.getLogger(WSLoteEnvio.class);
+	private final NFeConfig config;
 
-    public WSLoteEnvio(final NFeConfig config) {
-        this.config = config;
-    }
+	public WSLoteEnvio(final NFeConfig config) {
+		this.config = config;
+	}
 
-    public NFLoteEnvioRetorno enviaLoteAssinado(final String loteAssinadoXml) throws Exception {
-        XMLValidador.validaLote(loteAssinadoXml);
-        return this.comunicaLote(loteAssinadoXml, NFModelo.NFE);
-    }
+	public NFLoteEnvioRetorno enviaLoteAssinado(final String loteAssinadoXml) throws Exception {
+		XMLValidador.validaLote(loteAssinadoXml);
+		return this.comunicaLote(loteAssinadoXml, NFModelo.NFE);
+	}
 
-    public NFLoteEnvioRetorno enviaLote(final NFLoteEnvio lote) throws Exception {
-        // adiciona a chave e o dv antes de assinar
-        for (final NFNota nota : lote.getNotas()) {
-            final NFGeraChave geraChave = new NFGeraChave(nota);
-            final NFNotaInfo notaInfo = nota.getInfo();
-            notaInfo.setIdentificador(geraChave.getChaveAcesso());
-            notaInfo.getIdentificacao().setDigitoVerificador(geraChave.getDV());
-        }
+	public NFLoteEnvioRetorno enviaLote(final NFLoteEnvio lote) throws Exception {
+		// adiciona a chave e o dv antes de assinar
+		for (final NFNota nota : lote.getNotas()) {
+			final NFGeraChave geraChave = new NFGeraChave(nota);
+			final NFNotaInfo notaInfo = nota.getInfo();
+			notaInfo.setIdentificador(geraChave.getChaveAcesso());
+			notaInfo.getIdentificacao().setDigitoVerificador(geraChave.getDV());
+		}
 
-        // valida o lote gerado (ainda nao assinado)
-        XMLValidador.validaLote(lote.toString());
+		// valida o lote gerado (ainda nao assinado)
+		XMLValidador.validaLote(lote.toString());
 
-        // assina o lote
-        final String documentoAssinado = new AssinaturaDigital(this.config).assinarDocumento(lote.toString());
-        final NFLoteEnvio loteAssinado = new NotaParser().loteParaObjeto(documentoAssinado);
+		// assina o lote
+		final String documentoAssinado = new AssinaturaDigital(this.config).assinarDocumento(lote.toString());
+		final NFLoteEnvio loteAssinado = new NotaParser().loteParaObjeto(documentoAssinado);
 
-        // verifica se nao tem NFCe junto com NFe no lote e gera qrcode (apos assinar mesmo, eh assim)
-        int qtdNF = 0, qtdNFC = 0;
-        for (final NFNota nota : loteAssinado.getNotas()) {
-            switch (nota.getInfo().getIdentificacao().getModelo()) {
-                case NFE:
-                    qtdNF++;
-                    break;
-                case NFCE:
-                    final NFGeraQRCode geraQRCode = new NFGeraQRCode(nota, this.config);
-                    nota.setInfoSuplementar(new NFNotaInfoSuplementar());
-                    nota.getInfoSuplementar().setQrCode(geraQRCode.getQRCode());
-                    qtdNFC++;
-                    break;
-                default:
-                    throw new IllegalArgumentException(String.format("Modelo de nota desconhecida: %s", nota.getInfo().getIdentificacao().getModelo()));
-            }
-        }
+		// verifica se nao tem NFCe junto com NFe no lote e gera qrcode (apos assinar mesmo, eh assim)
+		int qtdNF = 0, qtdNFC = 0;
+		for (final NFNota nota : loteAssinado.getNotas()) {
+			switch (nota.getInfo().getIdentificacao().getModelo()) {
+				case NFE:
+					qtdNF++;
+					break;
+				case NFCE:
+					final NFGeraQRCode geraQRCode = new NFGeraQRCode(nota, this.config);
+					nota.setInfoSuplementar(new NFNotaInfoSuplementar());
+					nota.getInfoSuplementar().setQrCode(geraQRCode.getQRCode());
+					qtdNFC++;
+					break;
+				default:
+					throw new IllegalArgumentException(String.format("Modelo de nota desconhecida: %s", nota.getInfo().getIdentificacao().getModelo()));
+			}
+		}
 
-        // verifica se todas as notas do lote sao do mesmo modelo
-        if ((qtdNF > 0) && (qtdNFC > 0)) {
-            throw new IllegalArgumentException("Lote contendo notas de modelos diferentes!");
-        }
+		// verifica se todas as notas do lote sao do mesmo modelo
+		if ((qtdNF > 0) && (qtdNFC > 0)) {
+			throw new IllegalArgumentException("Lote contendo notas de modelos diferentes!");
+		}
 
-        // guarda o modelo das notas
-        final NFModelo modelo = qtdNFC > 0 ? NFModelo.NFCE : NFModelo.NFE;
+		// guarda o modelo das notas
+		final NFModelo modelo = qtdNFC > 0 ? NFModelo.NFCE : NFModelo.NFE;
 
-        // comunica o lote
-        return this.comunicaLote(loteAssinado.toString(), modelo);
-    }
+		// comunica o lote
+		return this.comunicaLote(loteAssinado.toString(), modelo);
+	}
 
-    private NFLoteEnvioRetorno comunicaLote(final String loteAssinadoXml, final NFModelo modelo) throws XMLStreamException, RemoteException, AxisFault, Exception {
-        final OMElement omElement = this.nfeToOMElement(loteAssinadoXml);
+	private NFLoteEnvioRetorno comunicaLote(final String loteAssinadoXml, final NFModelo modelo) throws XMLStreamException, RemoteException, AxisFault, Exception {
+		final OMElement omElement = this.nfeToOMElement(loteAssinadoXml);
 
-        final NfeDadosMsg dados = new NfeDadosMsg();
-        dados.setExtraElement(omElement);
+		final NfeDadosMsg dados = new NfeDadosMsg();
+		dados.setExtraElement(omElement);
 
-        final NfeCabecMsgE cabecalhoSOAP = this.getCabecalhoSOAP();
-        WSLoteEnvio.LOG.info(omElement);
+		final NfeCabecMsgE cabecalhoSOAP = this.getCabecalhoSOAP();
+		WSLoteEnvio.LOG.info(omElement);
 
-        final NFAutorizador31 autorizador = NFAutorizador31.valueOfCodigoUF(this.config.getCUF());
-        final String endpoint = NFModelo.NFE.equals(modelo) ? autorizador.getNfeAutorizacao(this.config.getAmbiente()) : autorizador.getNfceAutorizacao(this.config.getAmbiente());
-        if (endpoint == null) {
-            throw new IllegalArgumentException("Nao foi possivel encontrar URL para Autorizacao " + modelo.name() + ", autorizador " + autorizador.name());
-        }
+		final NFAutorizador31 autorizador = NFAutorizador31.valueOfCodigoUF(this.config.getCUF());
+		final String endpoint = NFModelo.NFE.equals(modelo) ? autorizador.getNfeAutorizacao(this.config.getAmbiente()) : autorizador.getNfceAutorizacao(this.config.getAmbiente());
+		if (endpoint == null) {
+			throw new IllegalArgumentException("Nao foi possivel encontrar URL para Autorizacao " + modelo.name() + ", autorizador " + autorizador.name());
+		}
 
-        final NfeAutorizacaoLoteResult autorizacaoLoteResult = new NfeAutorizacaoStub(endpoint).nfeAutorizacaoLote(dados, cabecalhoSOAP);
-        final NFLoteEnvioRetorno loteEnvioRetorno = new NFPersister().read(NFLoteEnvioRetorno.class, autorizacaoLoteResult.getExtraElement().toString());
-        WSLoteEnvio.LOG.info(loteEnvioRetorno.toString());
-        return loteEnvioRetorno;
-    }
+		final NfeAutorizacaoLoteResult autorizacaoLoteResult = new NfeAutorizacaoStub(endpoint).nfeAutorizacaoLote(dados, cabecalhoSOAP);
+		final NFLoteEnvioRetorno loteEnvioRetorno = new NFPersister().read(NFLoteEnvioRetorno.class, autorizacaoLoteResult.getExtraElement().toString());
+		WSLoteEnvio.LOG.info(loteEnvioRetorno.toString());
+		return loteEnvioRetorno;
+	}
 
-    private NfeCabecMsgE getCabecalhoSOAP() {
-        final NfeCabecMsg cabecalho = new NfeCabecMsg();
-        cabecalho.setCUF(this.config.getCUF().getCodigoIbge());
-        cabecalho.setVersaoDados(NFeConfig.VERSAO_NFE);
-        final NfeCabecMsgE cabecalhoSOAP = new NfeCabecMsgE();
-        cabecalhoSOAP.setNfeCabecMsg(cabecalho);
-        return cabecalhoSOAP;
-    }
+	private NfeCabecMsgE getCabecalhoSOAP() {
+		final NfeCabecMsg cabecalho = new NfeCabecMsg();
+		cabecalho.setCUF(this.config.getCUF().getCodigoIbge());
+		cabecalho.setVersaoDados(NFeConfig.VERSAO_NFE);
+		final NfeCabecMsgE cabecalhoSOAP = new NfeCabecMsgE();
+		cabecalhoSOAP.setNfeCabecMsg(cabecalho);
+		return cabecalhoSOAP;
+	}
 
-    private OMElement nfeToOMElement(final String documento) throws XMLStreamException {
-        final OMElement ome = AXIOMUtil.stringToOM(documento);
-        final Iterator<?> children = ome.getChildrenWithLocalName(WSLoteEnvio.NFE_ELEMENTO);
-        while (children.hasNext()) {
-            final OMElement omElement = (OMElement) children.next();
-            if ((omElement != null) && (WSLoteEnvio.NFE_ELEMENTO.equals(omElement.getLocalName()))) {
-                omElement.addAttribute("xmlns", NFeConfig.NFE_NAMESPACE, null);
-            }
-        }
-        return ome;
-    }
+	private OMElement nfeToOMElement(final String documento) throws XMLStreamException {
+		final OMElement ome = AXIOMUtil.stringToOM(documento);
+		final Iterator<?> children = ome.getChildrenWithLocalName(WSLoteEnvio.NFE_ELEMENTO);
+		while (children.hasNext()) {
+			final OMElement omElement = (OMElement) children.next();
+			if ((omElement != null) && (WSLoteEnvio.NFE_ELEMENTO.equals(omElement.getLocalName()))) {
+				omElement.addAttribute("xmlns", NFeConfig.NFE_NAMESPACE, null);
+			}
+		}
+		return ome;
+	}
 }
