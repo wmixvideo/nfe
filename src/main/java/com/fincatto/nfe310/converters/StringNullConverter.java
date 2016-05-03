a3e8c3685933c82e36cd3401fbce256684b789d3
Diego Fincatto <diego@fincatto.com>
2016-05-03 09:52:59 -0300

    Corrigido geracao de lote cEAN, quando nao vazio.

--- src/main/java/com/fincatto/nfe310/converters/StringNullConverter.java
+++ src/main/java/com/fincatto/nfe310/converters/StringNullConverter.java
@@ -1,18 +1,20 @@
 package com.fincatto.nfe310.converters;
 
+import org.apache.commons.lang3.StringUtils;
 import org.simpleframework.xml.convert.Converter;
 import org.simpleframework.xml.stream.InputNode;
 import org.simpleframework.xml.stream.OutputNode;
 
 public class StringNullConverter implements Converter<String> {
 
-    @Override
-    public String read(final InputNode node) throws Exception {
-        return node.getValue() == null ? "" : node.getValue();
-    }
+	@Override
+	public String read(final InputNode node) throws Exception {
+		final String value = node.getValue(); //estranhamente, ao pegar a primeira vez, na segunda ele retorna nulo (deixar setando a variavel)
+		return StringUtils.isNotBlank(value) ? value : StringUtils.EMPTY;
+	}
 
-    @Override
-    public void write(final OutputNode node, final String value) throws Exception {
-        node.setValue(value);
-    }
+	@Override
+	public void write(final OutputNode node, final String value) throws Exception {
+		node.setValue(value);
+	}
 }
