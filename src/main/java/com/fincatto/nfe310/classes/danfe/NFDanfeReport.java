07a2b1d17276d1a68d22d6e6bdb8556506b60f9e
felipe.martins <felipewmartins@gmail.com>
2016-05-23 22:53:47 -0300

    Testes DANFE e layout

--- src/main/java/com/fincatto/nfe310/classes/danfe/NFDanfeReport.java
+++ src/main/java/com/fincatto/nfe310/classes/danfe/NFDanfeReport.java
@@ -1,6 +1,11 @@
 package com.fincatto.nfe310.classes.danfe;
 
+import java.io.InputStream;
+import java.io.StringReader;
 import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
@@ -9,28 +14,46 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.data.JRXmlDataSource;
+import net.sf.jasperreports.engine.util.JRXmlUtils;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 import com.fincatto.nfe310.classes.nota.NFNota;
 
 public class NFDanfeReport {
-	private static ArrayList<NFNota> notaList;
-
-	public static void imprimirDanfe(NFNota notaProcessadaSefaz,
+	private static ClassLoader classloader;
+	private static Document document;
+	
+	public static boolean imprimirDanfe(NFNota xmlNota,
 			String pathDestino) {
-		notaList.add(notaProcessadaSefaz);
+		
+	    
+	    try {
+	    	String tst = xmlNota.toString();
+	    	System.out.println(tst);
+			document = JRXmlUtils.parse(new InputSource( new StringReader(xmlNota.toString()))); 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
+		boolean retorno = false;
 		try {
-			JasperReport report = JasperCompileManager
-					.compileReport("danfe/DANFE.jrxml");
+			classloader = Thread.currentThread().getContextClassLoader();
+			InputStream in = classloader.getResourceAsStream("danfe/danfeR2.jrxml");
+			JasperReport report = JasperCompileManager.compileReport(in);
+			System.out.println("Passei por aqui");
 			JasperPrint print = JasperFillManager.fillReport(report, null,
-					new JRBeanCollectionDataSource(notaList));
+					new JRXmlDataSource(document,"/NFe/infNFe/det"));
 			JasperExportManager.exportReportToPdfFile(print, pathDestino);
 			System.out.println("Danfe impresso em: " + pathDestino);
-
+			retorno = true;
 		} catch (JRException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return retorno;
 
 	}
 
