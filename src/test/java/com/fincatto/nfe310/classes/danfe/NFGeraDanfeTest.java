1877147d2c6eaf1e46b0715bd6e22881e4a5727d
felipe.martins <felipewmartins@gmail.com>
2016-06-12 11:29:16 -0300

    Alterando retorno do método de geração do danfe para byte[]. Ajustando teste de existencia do arquivo.

--- src/test/java/com/fincatto/nfe310/classes/danfe/NFGeraDanfeTest.java
+++ src/test/java/com/fincatto/nfe310/classes/danfe/NFGeraDanfeTest.java
@@ -1,5 +1,10 @@
 package com.fincatto.nfe310.classes.danfe;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.fincatto.nfe310.FabricaDeObjetosFake;
@@ -10,7 +15,7 @@ import com.fincatto.nfe310.classes.nota.assinatura.NFSignature;
 public class NFGeraDanfeTest {
 
 	@Test
-	public void deveGerarDanfeAPartirDoXML() {
+	public void deveGerarDanfeAPartirDoXML() throws IOException {
 		final NFNota nota = new NFNota();
 		final NFSignature assinatura = new NFSignature();
 		assinatura.setSignatureValue("NFe89172658591754401086218048846976493475937081");
@@ -20,8 +25,10 @@ public class NFGeraDanfeTest {
 		nota.setInfo(notaInfo);
 		nota.setIdentificadorLocal(identificadorLocal);
 		nota.toString();
-		NFDanfeReport.imprimirDanfe(nota, "/home/felipe/teste.pdf");
-		//Assert.assertTrue(NFDanfeReport.imprimirDanfe(nota, "/tmp/"));
+		FileOutputStream fos = new FileOutputStream("/tmp/danfe.pdf");
+		fos.write(NFDanfeReport.imprimirDanfe(nota));
+		fos.close();
+		Assert.assertTrue(new File("/tmp/danfe.pdf").exists());
 	}
 
 }
