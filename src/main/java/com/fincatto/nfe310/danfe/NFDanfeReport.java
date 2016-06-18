26d489fe647c039fa049dc1e07ba43f79bc4f381
Diego Fincatto <diego@fincatto.com>
2016-06-18 16:55:32 -0300

    Ajustado codigo da geracao de danfe.

--- src/main/java/com/fincatto/nfe310/danfe/NFDanfeReport.java
+++ src/main/java/com/fincatto/nfe310/danfe/NFDanfeReport.java
@@ -1,55 +1,37 @@
 package com.fincatto.nfe310.danfe;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringReader;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
+import com.fincatto.nfe310.classes.nota.NFNotaProcessada;
 import net.sf.jasperreports.engine.DefaultJasperReportsContext;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRPropertiesUtil;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReportsContext;
 import net.sf.jasperreports.engine.data.JRXmlDataSource;
-
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-import com.fincatto.nfe310.classes.nota.NFNotaProcessada;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
 
-public class NFDanfeReport {
+public abstract class NFDanfeReport {
 
-	public static byte[] imprimirDanfe(NFNotaProcessada xmlNota) throws JRException,
-			ParserConfigurationException, SAXException, IOException {
+    public static byte[] imprimirDanfe(NFNotaProcessada nota) throws IOException, ParserConfigurationException, SAXException, JRException {
+        try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("danfe/danfeR3.jasper")) {
+            final JRPropertiesUtil jrPropertiesUtil = JRPropertiesUtil.getInstance(DefaultJasperReportsContext.getInstance());
+            jrPropertiesUtil.setProperty("net.sf.jasperreports.xpath.executer.factory", "net.sf.jasperreports.engine.util.xml.JaxenXPathExecuterFactory");
+            return JasperExportManager.exportReportToPdf(JasperFillManager.fillReport(in, null, new JRXmlDataSource(convertStringXMl2DOM(nota.toString()), "/nfeProc/NFe/infNFe/det")));
+        }
+    }
 
-		ClassLoader classloader = Thread.currentThread()
-				.getContextClassLoader();
-		byte[] reportByte;
-		try (InputStream in = classloader.getResourceAsStream("danfe/danfeR3.jasper")) {
-			JasperReportsContext jasperReportsContext = DefaultJasperReportsContext.getInstance();
-			JRPropertiesUtil jrPropertiesUtil = JRPropertiesUtil.getInstance(jasperReportsContext);
-			jrPropertiesUtil.setProperty("net.sf.jasperreports.xpath.executer.factory",
-					"net.sf.jasperreports.engine.util.xml.JaxenXPathExecuterFactory");
-			JasperPrint print = JasperFillManager.fillReport(in, null,
-					new JRXmlDataSource(convertStringXMl2DOM(xmlNota.toString()),"/nfeProc/NFe/infNFe/det"));
-			reportByte = JasperExportManager.exportReportToPdf(print);
-		}
-		return reportByte;
-	}
-
-	private static Document convertStringXMl2DOM(String nota) throws ParserConfigurationException, SAXException, IOException {
-		Document doc = null;
-		DocumentBuilder db = DocumentBuilderFactory.newInstance()
-				.newDocumentBuilder();
-		InputSource is = new InputSource();
-		is.setCharacterStream(new StringReader(nota));
-		doc = db.parse(is);
-		return doc;
-	}
+    private static Document convertStringXMl2DOM(String nota) throws ParserConfigurationException, IOException, SAXException {
+        try (StringReader stringReader = new StringReader(nota)) {
+            InputSource inputSource = new InputSource();
+            inputSource.setCharacterStream(stringReader);
+            return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(inputSource);
+        }
+    }
 }
--- src/test/java/com/fincatto/nfe310/danfe/NFDanfeReportTest.java
+++ src/test/java/com/fincatto/nfe310/danfe/NFDanfeReportTest.java
@@ -1,37 +1,38 @@
 package com.fincatto.nfe310.danfe;
 
-import java.math.BigDecimal;
-
-import org.junit.Assert;
-import org.junit.Test;
-
 import com.fincatto.nfe310.FabricaDeObjetosFake;
 import com.fincatto.nfe310.classes.NFProtocolo;
 import com.fincatto.nfe310.classes.nota.NFNota;
-import com.fincatto.nfe310.classes.nota.NFNotaInfo;
 import com.fincatto.nfe310.classes.nota.NFNotaProcessada;
 import com.fincatto.nfe310.classes.nota.assinatura.NFSignature;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
 
 public class NFDanfeReportTest {
 
-	@Test
-	public void deveGerarDanfeAPartirDoXML() throws Exception {
-		final NFNota nota = new NFNota();
-		final NFNotaProcessada notaProcessada = new NFNotaProcessada();
-		final NFSignature assinatura = new NFSignature();
-		assinatura.setSignatureValue("NFe89172658591754401086218048846976493475937081");
-		final NFNotaInfo notaInfo = FabricaDeObjetosFake.getNFNotaInfo();
-		final int identificadorLocal = 123456;
-		final NFProtocolo nfProtocolo = new NFProtocolo();
-		nota.setAssinatura(assinatura);
-		nota.setInfo(notaInfo);
-		nota.setIdentificadorLocal(identificadorLocal);
-		notaProcessada.setNota(nota);
-		notaProcessada.setVersao(new BigDecimal(10));
-		nfProtocolo.setProtocoloInfo(FabricaDeObjetosFake.getNFProtocoloInfo());
-		nfProtocolo.setVersao("3.10");
-		notaProcessada.setProtocolo(nfProtocolo);
+    @Test
+    public void deveGerarDanfeAPartirDoXML() throws Exception {
+        final NFProtocolo nfProtocolo = new NFProtocolo();
+        nfProtocolo.setProtocoloInfo(FabricaDeObjetosFake.getNFProtocoloInfo());
+        nfProtocolo.setVersao("3.10");
 
-		Assert.assertFalse(NFDanfeReport.imprimirDanfe(notaProcessada).length == 0);
-	}
+        final NFSignature signature = new NFSignature();
+        signature.setSignatureValue("NFe89172658591754401086218048846976493475937081");
+
+        final NFNota nota = new NFNota();
+        nota.setInfo(FabricaDeObjetosFake.getNFNotaInfo());
+        nota.setIdentificadorLocal(123456);
+        nota.setAssinatura(signature);
+
+        final NFNotaProcessada notaProcessada = new NFNotaProcessada();
+        notaProcessada.setVersao(BigDecimal.TEN);
+        notaProcessada.setProtocolo(nfProtocolo);
+        notaProcessada.setNota(nota);
+
+        final byte[] danfe = NFDanfeReport.imprimirDanfe(notaProcessada);
+        //FileUtils.writeByteArrayToFile(new File("/tmp/danfe.pdf"), danfe);
+        Assert.assertTrue(danfe.length > 0);
+    }
 }
