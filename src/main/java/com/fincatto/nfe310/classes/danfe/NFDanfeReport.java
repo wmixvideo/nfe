f8e10ce0f2c57585158c9c568c8c08574bafece6
felipe.martins <felipewmartins@gmail.com>
2016-06-17 21:51:12 -0300

    Ajustando objeto de geração do danfe e usando jasper ao invés de xml

--- src/main/java/com/fincatto/nfe310/classes/danfe/NFDanfeReport.java
+++ src/main/java/com/fincatto/nfe310/classes/danfe/NFDanfeReport.java
@@ -8,38 +8,36 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import net.sf.jasperreports.engine.DefaultJasperReportsContext;
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JRPropertiesUtil;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JasperReportsContext;
 import net.sf.jasperreports.engine.data.JRXmlDataSource;
-import net.sf.jasperreports.engine.util.JRProperties;
 
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-import com.fincatto.nfe310.classes.nota.NFNota;
+import com.fincatto.nfe310.classes.nota.NFNotaProcessada;
 
 public class NFDanfeReport {
 
-	public static byte[] imprimirDanfe(NFNota xmlNota) throws JRException,
+	public static byte[] imprimirDanfe(NFNotaProcessada xmlNota) throws JRException,
 			ParserConfigurationException, SAXException, IOException {
 
 		ClassLoader classloader = Thread.currentThread()
 				.getContextClassLoader();
 		byte[] reportByte;
-		try (InputStream in = classloader.getResourceAsStream("danfe/danfeR3.jrxml")) {
-			JasperReport report = JasperCompileManager.compileReport(in);
-			JRProperties
-					.setProperty("net.sf.jasperreports.xpath.executer.factory",
-							"net.sf.jasperreports.engine.util.xml.JaxenXPathExecuterFactory");
-			JasperPrint print = JasperFillManager.fillReport(report, null,
-					new JRXmlDataSource(
-							convertStringXMl2DOM(xmlNota.toString()),
-							"/NFe/infNFe/det"));
+		try (InputStream in = classloader.getResourceAsStream("danfe/danfeR3.jasper")) {
+			JasperReportsContext jasperReportsContext = DefaultJasperReportsContext.getInstance();
+			JRPropertiesUtil jrPropertiesUtil = JRPropertiesUtil.getInstance(jasperReportsContext);
+			jrPropertiesUtil.setProperty("net.sf.jasperreports.xpath.executer.factory",
+					"net.sf.jasperreports.engine.util.xml.JaxenXPathExecuterFactory");
+			JasperPrint print = JasperFillManager.fillReport(in, null,
+					new JRXmlDataSource(convertStringXMl2DOM(xmlNota.toString()),"/nfeProc/NFe/infNFe/det"));
 			reportByte = JasperExportManager.exportReportToPdf(print);
 		}
 		return reportByte;
