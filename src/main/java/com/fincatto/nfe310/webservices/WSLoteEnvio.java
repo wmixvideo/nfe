8487501ea5dafcf1d4838d05c4c87b0482a56e34
André Gusmão <andredgusmao@gmail.com>
2016-10-10 16:58:48 -0300

    Removido imports não utilizados.

--- src/main/java/com/fincatto/nfe310/webservices/WSLoteEnvio.java
+++ src/main/java/com/fincatto/nfe310/webservices/WSLoteEnvio.java
@@ -5,7 +5,6 @@ import java.util.Iterator;
 import javax.xml.stream.XMLStreamException;
 
 import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.util.AXIOMUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
