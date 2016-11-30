b7c4f0559360fd831d22238573d3d2d3d5358e85
Edivaldo <edivaldo@gmail.com>
2016-11-30 00:23:07 -0200

    Adicionado o metodo getChave() para facilitar na reutilização da chave já setada caso tenha que utilizar em outras partes do código.

--- src/main/java/com/fincatto/nfe310/parsers/NotaFiscalChaveParser.java
+++ src/main/java/com/fincatto/nfe310/parsers/NotaFiscalChaveParser.java
@@ -8,72 +8,76 @@ import org.joda.time.LocalDate;
 
 public class NotaFiscalChaveParser {
 
-    private final String chave;
+  private final String chave;
 
-    public NotaFiscalChaveParser(final String chave) {
-        this.chave = StringUtils.stripToEmpty(chave).replaceAll("\\D", "");
-        if (this.chave.length() != 44) {
-            throw new IllegalArgumentException(String.format("A chave deve ter exatos 44 caracteres numericos: %s", chave));
-        }
+  public NotaFiscalChaveParser(final String chave) {
+    this.chave = StringUtils.stripToEmpty(chave).replaceAll("\\D", "");
+    if (this.chave.length() != 44) {
+      throw new IllegalArgumentException(String.format("A chave deve ter exatos 44 caracteres numericos: %s", chave));
     }
+  }
 
-    public NFUnidadeFederativa getNFUnidadeFederativa() {
-        return NFUnidadeFederativa.valueOfCodigo(this.chave.substring(0, 2));
-    }
+  public String getChave() {
+    return chave;
+  }
 
-    public LocalDate getDataEmissao() {
-        return new LocalDate(this.getDataEmissaoAno(), this.getDataEmissaoMes(), 1);
-    }
+  public NFUnidadeFederativa getNFUnidadeFederativa() {
+    return NFUnidadeFederativa.valueOfCodigo(this.chave.substring(0, 2));
+  }
 
-    private int getDataEmissaoMes() {
-        return Integer.parseInt(this.chave.substring(4, 6));
-    }
+  public LocalDate getDataEmissao() {
+    return new LocalDate(this.getDataEmissaoAno(), this.getDataEmissaoMes(), 1);
+  }
 
-    private int getDataEmissaoAno() {
-        return 2000 + Integer.parseInt(this.chave.substring(2, 4));
-    }
+  private int getDataEmissaoMes() {
+    return Integer.parseInt(this.chave.substring(4, 6));
+  }
 
-    public String getCnpjEmitente() {
-        return this.chave.substring(6, 20);
-    }
+  private int getDataEmissaoAno() {
+    return 2000 + Integer.parseInt(this.chave.substring(2, 4));
+  }
 
-    public NFModelo getModelo() {
-        return NFModelo.valueOfCodigo(this.chave.substring(20, 22));
-    }
+  public String getCnpjEmitente() {
+    return this.chave.substring(6, 20);
+  }
 
-    public String getSerie() {
-        return this.chave.substring(22, 25);
-    }
+  public NFModelo getModelo() {
+    return NFModelo.valueOfCodigo(this.chave.substring(20, 22));
+  }
 
-    public String getNumero() {
-        return this.chave.substring(25, 34);
-    }
+  public String getSerie() {
+    return this.chave.substring(22, 25);
+  }
 
-    public NFTipoEmissao getFormaEmissao() {
-        return NFTipoEmissao.valueOfCodigo(this.chave.substring(34, 35));
-    }
+  public String getNumero() {
+    return this.chave.substring(25, 34);
+  }
 
-    public String getCodigoNumerico() {
-        return this.chave.substring(35, 43);
-    }
+  public NFTipoEmissao getFormaEmissao() {
+    return NFTipoEmissao.valueOfCodigo(this.chave.substring(34, 35));
+  }
 
-    public String getDV() {
-        return this.chave.substring(43, 44);
-    }
+  public String getCodigoNumerico() {
+    return this.chave.substring(35, 43);
+  }
 
-    public boolean isEmitidaContingenciaSCAN() {
-        return this.getSerie().matches("9[0-9]{2}");
-    }
+  public String getDV() {
+    return this.chave.substring(43, 44);
+  }
 
-    public boolean isEmitidaContingenciaSCVAN() {
-        return this.chave.matches("\\d{34}6\\d{9}");
-    }
+  public boolean isEmitidaContingenciaSCAN() {
+    return this.getSerie().matches("9[0-9]{2}");
+  }
 
-    public boolean isEmitidaContingenciaSCVRS() {
-        return this.chave.matches("\\d{34}7\\d{9}");
-    }
+  public boolean isEmitidaContingenciaSCVAN() {
+    return this.chave.matches("\\d{34}6\\d{9}");
+  }
 
-    public String getFormatado() {
-        return this.chave.replaceFirst("(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})", "$1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11");
-    }
+  public boolean isEmitidaContingenciaSCVRS() {
+    return this.chave.matches("\\d{34}7\\d{9}");
+  }
+
+  public String getFormatado() {
+    return this.chave.replaceFirst("(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{4})", "$1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11");
+  }
 }
