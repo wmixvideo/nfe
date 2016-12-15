cc6dabf409754ff2f3c94bd8b6e033b315ae69b8
Edivaldo <edivaldo@gmail.com>
2016-12-15 21:27:43 -0200

    Formatação/indentação do código de acordo com o projeto.

--- src/main/java/com/fincatto/nfe310/classes/evento/cancelamento/NFProtocoloEventoCancelamento.java
+++ src/main/java/com/fincatto/nfe310/classes/evento/cancelamento/NFProtocoloEventoCancelamento.java
@@ -12,36 +12,36 @@ import org.simpleframework.xml.Root;
 @Namespace(reference = "http://www.portalfiscal.inf.br/nfe")
 public class NFProtocoloEventoCancelamento extends NFBase {
 
-  @Attribute(name = "versao", required = true)
-  private String versao;
+    @Attribute(name = "versao", required = true)
+    private String versao;
 
-  @Element(name = "evento", required = true)
-  private NFEventoCancelamento evento;
+    @Element(name = "evento", required = true)
+    private NFEventoCancelamento evento;
 
-  @Element(name = "retEvento", required = true)
-  private NFEventoRetorno eventoRetorno;
+    @Element(name = "retEvento", required = true)
+    private NFEventoRetorno eventoRetorno;
 
-  public NFEventoCancelamento getEvento() {
-    return this.evento;
-  }
+    public NFEventoCancelamento getEvento() {
+        return this.evento;
+    }
 
-  public void setEvento(final NFEventoCancelamento evento) {
-    this.evento = evento;
-  }
+    public void setEvento(final NFEventoCancelamento evento) {
+        this.evento = evento;
+    }
 
-  public NFEventoRetorno getInfoEventoRetorno() {
-    return this.eventoRetorno;
-  }
+    public NFEventoRetorno getInfoEventoRetorno() {
+        return this.eventoRetorno;
+    }
 
-  public void setEventoRetorno(final NFEventoRetorno infoEventoRetorno) {
-    this.eventoRetorno = infoEventoRetorno;
-  }
+    public void setEventoRetorno(final NFEventoRetorno infoEventoRetorno) {
+        this.eventoRetorno = infoEventoRetorno;
+    }
 
-  public String getVersao() {
-    return this.versao;
-  }
+    public String getVersao() {
+        return this.versao;
+    }
 
-  public void setVersao(final String versao) {
-    this.versao = versao;
-  }
+    public void setVersao(final String versao) {
+        this.versao = versao;
+    }
 }
--- src/main/java/com/fincatto/nfe310/parsers/NotaParser.java
+++ src/main/java/com/fincatto/nfe310/parsers/NotaParser.java
@@ -14,105 +14,105 @@ import com.fincatto.nfe310.persister.NFPersister;
 
 public class NotaParser {
 
-  private final Persister persister;
+    private final Persister persister;
 
-  public NotaParser() {
-    this.persister = new NFPersister();
-  }
-
-  public NFNota notaParaObjeto(final File xml) {
-    try {
-      return this.persister.read(NFNota.class, xml);
-    } catch (final Exception e) {
-      throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+    public NotaParser() {
+        this.persister = new NFPersister();
     }
-  }
 
-  public NFNota notaParaObjeto(final String xml) {
-    try {
-      return this.persister.read(NFNota.class, xml);
-    } catch (final Exception e) {
-      throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+    public NFNota notaParaObjeto(final File xml) {
+        try {
+            return this.persister.read(NFNota.class, xml);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+        }
     }
-  }
 
-  public NFLoteEnvio loteParaObjeto(final File xml) {
-    try {
-      return this.persister.read(NFLoteEnvio.class, xml);
-    } catch (final Exception e) {
-      throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+    public NFNota notaParaObjeto(final String xml) {
+        try {
+            return this.persister.read(NFNota.class, xml);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+        }
     }
-  }
 
-  public NFLoteEnvio loteParaObjeto(final String xml) {
-    try {
-      return this.persister.read(NFLoteEnvio.class, xml);
-    } catch (final Exception e) {
-      throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+    public NFLoteEnvio loteParaObjeto(final File xml) {
+        try {
+            return this.persister.read(NFLoteEnvio.class, xml);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+        }
     }
-  }
 
-  public NFNotaProcessada notaProcessadaParaObjeto(final String xml) {
-    try {
-      return this.persister.read(NFNotaProcessada.class, xml);
-    } catch (final Exception e) {
-      throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+    public NFLoteEnvio loteParaObjeto(final String xml) {
+        try {
+            return this.persister.read(NFLoteEnvio.class, xml);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+        }
     }
-  }
 
-  public NFNotaProcessada notaProcessadaParaObjeto(final File xml) {
-    try {
-      return this.persister.read(NFNotaProcessada.class, xml);
-    } catch (final Exception e) {
-      throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+    public NFNotaProcessada notaProcessadaParaObjeto(final String xml) {
+        try {
+            return this.persister.read(NFNotaProcessada.class, xml);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+        }
     }
-  }
 
-  public NFEnviaEventoCartaCorrecao enviaEventoCartaCorrecaoParaObjeto(final File xml) {
-    try {
-      return this.persister.read(NFEnviaEventoCartaCorrecao.class, xml);
-    } catch (final Exception e) {
-      throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+    public NFNotaProcessada notaProcessadaParaObjeto(final File xml) {
+        try {
+            return this.persister.read(NFNotaProcessada.class, xml);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+        }
     }
-  }
 
-  public NFEnviaEventoCartaCorrecao enviaEventoCartaCorrecaoParaObjeto(final String xml) {
-    try {
-      return this.persister.read(NFEnviaEventoCartaCorrecao.class, xml);
-    } catch (final Exception e) {
-      throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+    public NFEnviaEventoCartaCorrecao enviaEventoCartaCorrecaoParaObjeto(final File xml) {
+        try {
+            return this.persister.read(NFEnviaEventoCartaCorrecao.class, xml);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+        }
     }
-  }
 
-  public NFEnviaEventoCancelamento enviaEventoCancelamentoParaObjeto(final File xml) {
-    try {
-      return this.persister.read(NFEnviaEventoCancelamento.class, xml);
-    } catch (final Exception e) {
-      throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+    public NFEnviaEventoCartaCorrecao enviaEventoCartaCorrecaoParaObjeto(final String xml) {
+        try {
+            return this.persister.read(NFEnviaEventoCartaCorrecao.class, xml);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+        }
     }
-  }
 
-  public NFEnviaEventoCancelamento enviaEventoCancelamentoParaObjeto(final String xml) {
-    try {
-      return this.persister.read(NFEnviaEventoCancelamento.class, xml);
-    } catch (final Exception e) {
-      throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+    public NFEnviaEventoCancelamento enviaEventoCancelamentoParaObjeto(final File xml) {
+        try {
+            return this.persister.read(NFEnviaEventoCancelamento.class, xml);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+        }
     }
-  }
 
-  public NFEnviaEventoInutilizacao enviaEventoInutilizacaoParaObjeto(final File xml) {
-    try {
-      return this.persister.read(NFEnviaEventoInutilizacao.class, xml);
-    } catch (final Exception e) {
-      throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+    public NFEnviaEventoCancelamento enviaEventoCancelamentoParaObjeto(final String xml) {
+        try {
+            return this.persister.read(NFEnviaEventoCancelamento.class, xml);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+        }
     }
-  }
 
-  public NFEnviaEventoInutilizacao enviaEventoInutilizacaoParaObjeto(final String xml) {
-    try {
-      return this.persister.read(NFEnviaEventoInutilizacao.class, xml);
-    } catch (final Exception e) {
-      throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+    public NFEnviaEventoInutilizacao enviaEventoInutilizacaoParaObjeto(final File xml) {
+        try {
+            return this.persister.read(NFEnviaEventoInutilizacao.class, xml);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+        }
     }
-  }
+
+    public NFEnviaEventoInutilizacao enviaEventoInutilizacaoParaObjeto(final String xml) {
+        try {
+            return this.persister.read(NFEnviaEventoInutilizacao.class, xml);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException(String.format("Nao foi possivel parsear o xml: %s", e.getMessage()));
+        }
+    }
 }
