9e466e301a579729420fbbadbcf1afd101a78d41
eldevan <eldevanjr@iservport.com>
2017-06-01 17:45:08 -0300

    Adiciona xsd para validações do MDFe

--- src/main/java/com/fincatto/nfe310/validadores/xsd/XMLValidador.java
+++ src/main/java/com/fincatto/nfe310/validadores/xsd/XMLValidador.java
@@ -27,4 +27,23 @@ public final class XMLValidador {
     public static boolean validaNota(final String arquivoXML) throws Exception {
         return XMLValidador.valida(arquivoXML, "nfe_v3.10.xsd");
     }
+
+    private static boolean validaMDF(final String xml, final String xsd) throws IOException, SAXException, URISyntaxException {
+        final URL xsdPath = XMLValidador.class.getClassLoader()
+                .getResource(String.format("schemas/PL_MDFe_300_NT022017/%s", xsd));
+        final SchemaFactory schemaFactory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+        final Schema schema = schemaFactory.newSchema(new StreamSource(xsdPath.toURI().toString()));
+        schema.newValidator().validate(new StreamSource(new StringReader(xml)));
+        return true;
+    }
+
+    public static boolean validaLoteMDFe(final String arquivoXML) throws Exception {
+        return XMLValidador.validaMDF(arquivoXML, "enviMDFe_v3.00.xsd");
+    }
+
+    public static boolean validaMDFe(final String arquivoXML) throws Exception {
+        return XMLValidador.validaMDF(arquivoXML, "mdfe_v3.00.xsd");
+    }
+
+
 }
