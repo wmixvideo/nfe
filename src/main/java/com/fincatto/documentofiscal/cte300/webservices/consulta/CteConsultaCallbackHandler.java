2620a1cfdc377ecda6f5827e7ac0c6143258afef
Caio <caiocteodoro@gmail.com>
2017-09-22 17:41:14 -0300

    xsd CTe 300

---
+++ src/main/java/com/fincatto/documentofiscal/cte300/webservices/consulta/CteConsultaCallbackHandler.java
@@ -0,0 +1,66 @@
+
+/**
+ * CteConsultaCallbackHandler.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
+ */
+
+    package com.fincatto.documentofiscal.cte300.webservices.consulta;
+
+    /**
+     *  CteConsultaCallbackHandler Callback class, Users can extend this class and implement
+     *  their own receiveResult and receiveError methods.
+     */
+    public abstract class CteConsultaCallbackHandler{
+
+
+
+    protected Object clientData;
+
+    /**
+    * User can pass in any object that needs to be accessed once the NonBlocking
+    * Web service call is finished and appropriate method of this CallBack is called.
+    * @param clientData Object mechanism by which the user can pass in user data
+    * that will be avilable at the time this callback is called.
+    */
+    public CteConsultaCallbackHandler(Object clientData){
+        this.clientData = clientData;
+    }
+
+    /**
+    * Please use this constructor if you don't want to set any clientData
+    */
+    public CteConsultaCallbackHandler(){
+        this.clientData = null;
+    }
+
+    /**
+     * Get the client data
+     */
+
+     public Object getClientData() {
+        return clientData;
+     }
+
+        
+           /**
+            * auto generated Axis2 call back method for cteConsultaCT method
+            * override this method for handling normal response from cteConsultaCT operation
+            */
+           public void receiveResultcteConsultaCT(
+                    com.fincatto.documentofiscal.cte300.webservices.consulta.CteConsultaStub.CteConsultaCTResult result
+                        ) {
+           }
+
+          /**
+           * auto generated Axis2 Error handler
+           * override this method for handling error response from cteConsultaCT operation
+           */
+            public void receiveErrorcteConsultaCT(java.lang.Exception e) {
+            }
+                
+
+
+    }
+    
---
+++ src/main/java/com/fincatto/documentofiscal/cte300/webservices/statusservico/CteStatusServicoCallbackHandler.java
@@ -0,0 +1,66 @@
+
+/**
+ * CteStatusServicoCallbackHandler.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
+ */
+
+    package com.fincatto.documentofiscal.cte300.webservices.statusservico;
+
+    /**
+     *  CteStatusServicoCallbackHandler Callback class, Users can extend this class and implement
+     *  their own receiveResult and receiveError methods.
+     */
+    public abstract class CteStatusServicoCallbackHandler{
+
+
+
+    protected Object clientData;
+
+    /**
+    * User can pass in any object that needs to be accessed once the NonBlocking
+    * Web service call is finished and appropriate method of this CallBack is called.
+    * @param clientData Object mechanism by which the user can pass in user data
+    * that will be avilable at the time this callback is called.
+    */
+    public CteStatusServicoCallbackHandler(Object clientData){
+        this.clientData = clientData;
+    }
+
+    /**
+    * Please use this constructor if you don't want to set any clientData
+    */
+    public CteStatusServicoCallbackHandler(){
+        this.clientData = null;
+    }
+
+    /**
+     * Get the client data
+     */
+
+     public Object getClientData() {
+        return clientData;
+     }
+
+        
+           /**
+            * auto generated Axis2 call back method for cteStatusServicoCT method
+            * override this method for handling normal response from cteStatusServicoCT operation
+            */
+           public void receiveResultcteStatusServicoCT(
+                    com.fincatto.documentofiscal.cte300.webservices.statusservico.CteStatusServicoStub.CteStatusServicoCTResult result
+                        ) {
+           }
+
+          /**
+           * auto generated Axis2 Error handler
+           * override this method for handling error response from cteStatusServicoCT operation
+           */
+            public void receiveErrorcteStatusServicoCT(java.lang.Exception e) {
+            }
+                
+
+
+    }
+    
