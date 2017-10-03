f6dc0b45f5d4b2c2c52eced04649335b81b9b839
Carine Bockorny <cbockorny@wmixvideo.com.br>
2017-10-03 11:54:29 -0300

    Ajustada versao da nfe para 3.10

--- src/main/java/com/fincatto/documentofiscal/nfe310/NFeConfig.java
+++ src/main/java/com/fincatto/documentofiscal/nfe310/NFeConfig.java
@@ -8,12 +8,11 @@ import com.fincatto.documentofiscal.nfe310.classes.NFTipoEmissao;
  */
 public abstract class NFeConfig extends DFConfig {
 
-    public static final String VERSAO = "3.00";
+    public static final String VERSAO = "3.10";
     public static final String NAMESPACE = "http://www.portalfiscal.inf.br/nfe";
-    
-	/**
+
+    /**
      * ID de contribuinte, somente para NFCe.
-     *
      * @return ID do contribuinte.
      */
     public Integer getCodigoSegurancaContribuinteID() {
@@ -22,16 +21,14 @@ public abstract class NFeConfig extends DFConfig {
 
     /**
      * Codigo de seguranca do contribuinte, com 36 caracteres, somente para NFCe.
-     *
      * @return Codigo de seguranca do contribuinte.
      */
     public String getCodigoSegurancaContribuinte() {
         return null;
     }
-    
+
     /**
      * Tipo da emissao das notas (se normal ou em contingencia).
-     *
      * @return Tipo da emissao das notas.
      */
     public NFTipoEmissao getTipoEmissao() {
