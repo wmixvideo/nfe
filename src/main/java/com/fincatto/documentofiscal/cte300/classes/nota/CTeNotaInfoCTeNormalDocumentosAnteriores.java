323d2f819da70e8f17df974d26bca2c0b9bb4ea2
eldevan <eldevanjr@iservport.com>
2017-10-10 09:22:06 -0300

    Corrige erro de anotação errada em classes CTe(@Element para @ElementList em List<>).

--- src/main/java/com/fincatto/documentofiscal/cte300/classes/nota/CTeNotaInfoCTeNormalDocumentosAnteriores.java
+++ src/main/java/com/fincatto/documentofiscal/cte300/classes/nota/CTeNotaInfoCTeNormalDocumentosAnteriores.java
@@ -1,12 +1,11 @@
 package com.fincatto.documentofiscal.cte300.classes.nota;
 
-import java.util.List;
-
-import org.simpleframework.xml.Element;
+import com.fincatto.documentofiscal.DFBase;
+import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Namespace;
 import org.simpleframework.xml.Root;
 
-import com.fincatto.documentofiscal.DFBase;
+import java.util.List;
 
 /**
  * @author Caio
@@ -18,7 +17,7 @@ import com.fincatto.documentofiscal.DFBase;
 public class CTeNotaInfoCTeNormalDocumentosAnteriores extends DFBase {
     private static final long serialVersionUID = 1244791382646017225L;
 
-    @Element(name = "emiDocAnt", required = true)
+    @ElementList(name = "emiDocAnt", inline = true)
     private List<CTeNotaInfoCTeNormalDocumentosAnterioresEmissorDocumentosAnteriores> emissorDocumentosAnteriores;
 
     public CTeNotaInfoCTeNormalDocumentosAnteriores() {
