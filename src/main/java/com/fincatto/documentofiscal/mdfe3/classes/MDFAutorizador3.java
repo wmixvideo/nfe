b132af2235ead62c9eda3fff8a3234522042aa40
eldevan <eldevanjr@iservport.com>
2017-11-13 13:49:34 -0200

    Corrige erro url MDF consulta status

--- src/main/java/com/fincatto/documentofiscal/mdfe3/classes/MDFAutorizador3.java
+++ src/main/java/com/fincatto/documentofiscal/mdfe3/classes/MDFAutorizador3.java
@@ -44,7 +44,7 @@ public enum MDFAutorizador3 {
         @Override
         public String getMDFeStatusServico(DFAmbiente ambiente) {
             return DFAmbiente.HOMOLOGACAO.equals(ambiente)
-                    ? "https://mdfe-homologacao.svrs.rs.gov.br/ws/MDFeStatusServico/MDFeStatusServico.asmx "
+                    ? "https://mdfe-homologacao.svrs.rs.gov.br/ws/MDFeStatusServico/MDFeStatusServico.asmx"
                     : "https://mdfe.svrs.rs.gov.br/ws/MDFeStatusServico/MDFeStatusServico.asmx";
         }
 
