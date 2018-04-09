29268424d67813c71394301beeabbd487683e62c
eldevan <eldevanjr@iservport.com>
2018-04-09 17:58:39 -0300

    Adiciona Boleto Bancário para tipso de pagamentos

--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoPagamento.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoPagamento.java
@@ -1,16 +1,15 @@
 package com.fincatto.documentofiscal.nfe400.classes.nota;
 
-import java.util.List;
-
-import org.simpleframework.xml.ElementList;
-
 import com.fincatto.documentofiscal.DFBase;
 import com.fincatto.documentofiscal.validadores.ListValidador;
+import org.simpleframework.xml.ElementList;
+
+import java.util.List;
 
 public class NFNotaInfoPagamento extends DFBase {
     private static final long serialVersionUID = -2613537583415054150L;
 
-    @ElementList(entry = "detPag", inline = true, required = true)
+    @ElementList(entry = "detPag", inline = true)
     private List<NFNotaInfoFormaPagamento> detalhamentoFormasPagamento;
 
     public void setDetalhamentoFormasPagamento(final List<NFNotaInfoFormaPagamento> detalhamentoFormasPagamento) {
