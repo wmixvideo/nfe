31ebde7bbb34e7dc972a07876e55e4a3765ddd39
Carine Bockorny <cbockorny@wmixvideo.com.br>
2018-05-09 14:01:57 -0300

    Add implements serializable.

--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/assinatura/NFSignatureMethod.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/assinatura/NFSignatureMethod.java
@@ -1,18 +1,21 @@
 package com.fincatto.documentofiscal.nfe400.classes.nota.assinatura;
 
+import java.io.Serializable;
+
 import org.simpleframework.xml.Attribute;
 
-public class NFSignatureMethod {
+public class NFSignatureMethod implements Serializable {
+    private static final long serialVersionUID = 4525403507445216913L;
 
-	@Attribute(name = "Algorithm", required = false)
+    @Attribute(name = "Algorithm", required = false)
     private String algorithm;
 
-	public String getAlgorithm() {
-		return algorithm;
-	}
+    public String getAlgorithm() {
+        return this.algorithm;
+    }
 
-	public void setAlgorithm(String algorithm) {
-		this.algorithm = algorithm;
-	}
-	
+    public void setAlgorithm(final String algorithm) {
+        this.algorithm = algorithm;
+    }
+
 }
