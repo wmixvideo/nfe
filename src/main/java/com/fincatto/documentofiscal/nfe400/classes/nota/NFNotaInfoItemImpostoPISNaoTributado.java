434b52eaeeaf2da9fa4eb5786add73d6f2178ef2
mauro <mauroalbrecht@ciss.com.br>
2018-11-30 12:00:14 -0200

    Adicionado OPERACAO_TRIBUTAVEL_SUBSTITUICAO_TRIBUTARIA
    Faltou colocar essa condição para que funcione de acordo com a receita
    federal.

--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoPISNaoTributado.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoPISNaoTributado.java
@@ -6,6 +6,7 @@ import com.fincatto.documentofiscal.DFBase;
 import com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoSituacaoTributariaPIS;
 
 public class NFNotaInfoItemImpostoPISNaoTributado extends DFBase {
+
     private static final long serialVersionUID = -961225710108754016L;
 
     @Element(name = "CST", required = true)
@@ -15,14 +16,19 @@ public class NFNotaInfoItemImpostoPISNaoTributado extends DFBase {
         this.situacaoTributaria = null;
     }
 
+    public NFNotaInfoSituacaoTributariaPIS getSituacaoTributaria() {
+        return this.situacaoTributaria;
+    }
+
     public void setSituacaoTributaria(final NFNotaInfoSituacaoTributariaPIS situacaoTributaria) {
-        if (!NFNotaInfoSituacaoTributariaPIS.OPERACAO_TRIBUTAVEL_MONOFASICA_ALIQUOTA_ZERO.equals(situacaoTributaria) && !NFNotaInfoSituacaoTributariaPIS.OPERACAO_TRIBUTAVEL_ALIQUOTA_ZERO.equals(situacaoTributaria) && !NFNotaInfoSituacaoTributariaPIS.OPERACAO_ISENTA_CONTRIBUICAO.equals(situacaoTributaria) && !NFNotaInfoSituacaoTributariaPIS.OPERACAO_SEM_INCIDENCIA_CONTRIBUICAO.equals(situacaoTributaria) && !NFNotaInfoSituacaoTributariaPIS.OPERACAO_COM_SUSPENSAO_CONTRIBUICAO.equals(situacaoTributaria)) {
+        if (!NFNotaInfoSituacaoTributariaPIS.OPERACAO_TRIBUTAVEL_MONOFASICA_ALIQUOTA_ZERO.equals(situacaoTributaria)
+                && !NFNotaInfoSituacaoTributariaPIS.OPERACAO_TRIBUTAVEL_ALIQUOTA_ZERO.equals(situacaoTributaria)
+                && !NFNotaInfoSituacaoTributariaPIS.OPERACAO_TRIBUTAVEL_SUBSTITUICAO_TRIBUTARIA.equals(situacaoTributaria)
+                && !NFNotaInfoSituacaoTributariaPIS.OPERACAO_ISENTA_CONTRIBUICAO.equals(situacaoTributaria)
+                && !NFNotaInfoSituacaoTributariaPIS.OPERACAO_SEM_INCIDENCIA_CONTRIBUICAO.equals(situacaoTributaria)
+                && !NFNotaInfoSituacaoTributariaPIS.OPERACAO_COM_SUSPENSAO_CONTRIBUICAO.equals(situacaoTributaria)) {
             throw new IllegalStateException("Situacao tributaria invalida no item PIS nao tributado");
         }
         this.situacaoTributaria = situacaoTributaria;
-    }
-
-    public NFNotaInfoSituacaoTributariaPIS getSituacaoTributaria() {
-        return this.situacaoTributaria;
     }
 }
