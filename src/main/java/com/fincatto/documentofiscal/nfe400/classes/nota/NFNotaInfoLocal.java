63e32d3f24cf2beec51ae8909c5832504320e070
walter <walter@softlandsistemas.com.br>
2019-05-07 19:14:56 -0300

    Criação do campo vICMSSubstituto criado recentemente na NT 2018-005.
    Ajuste conforme mudança de nome de um objeto.

--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoLocal.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoLocal.java
@@ -2,7 +2,7 @@ package com.fincatto.documentofiscal.nfe400.classes.nota;
 
 import com.fincatto.documentofiscal.DFBase;
 import com.fincatto.documentofiscal.DFUnidadeFederativa;
-import com.fincatto.documentofiscal.nfe400.converters.StringNullConverter;
+import com.fincatto.documentofiscal.nfe400.converters.NFStringNullToEmptyConverter;
 import com.fincatto.documentofiscal.validadores.StringValidador;
 import org.apache.commons.lang3.StringUtils;
 import org.simpleframework.xml.Element;
@@ -12,7 +12,7 @@ public class NFNotaInfoLocal extends DFBase {
     private static final long serialVersionUID = -6618642990785758823L;
 
     @Element(name = "CNPJ", required = false)
-    @Convert(StringNullConverter.class)
+    @Convert(NFStringNullToEmptyConverter.class)
     private String cnpj;
 
     @Element(name = "CPF", required = false)
