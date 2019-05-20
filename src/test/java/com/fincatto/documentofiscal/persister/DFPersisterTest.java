0c860c17ce32202d8351f9ace783c45b56c4e282
Marcos <marcoslombardi@gmail.com>
2019-05-20 17:03:59 -0300

    Ajustes testes para DFPersister() modo estrito

--- src/test/java/com/fincatto/documentofiscal/persister/DFPersisterTest.java
+++ src/test/java/com/fincatto/documentofiscal/persister/DFPersisterTest.java
@@ -1,18 +1,21 @@
 package com.fincatto.documentofiscal.persister;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URI;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.simpleframework.xml.core.ElementException;
+import org.simpleframework.xml.core.Persister;
+
 import com.fincatto.documentofiscal.nfe310.FabricaDeObjetosFake;
 import com.fincatto.documentofiscal.nfe400.classes.evento.cancelamento.NFEnviaEventoCancelamento;
 import com.fincatto.documentofiscal.nfe400.classes.evento.cartacorrecao.NFEnviaEventoCartaCorrecao;
 import com.fincatto.documentofiscal.nfe400.classes.evento.inutilizacao.NFEnviaEventoInutilizacao;
 import com.fincatto.documentofiscal.nfe400.classes.nota.NFNota;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.simpleframework.xml.core.Persister;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.net.URI;
 
 public class DFPersisterTest {
     
@@ -31,6 +34,33 @@ public class DFPersisterTest {
     @Test(expected = FileNotFoundException.class)
     public void deveLancarExcecaoCasoRecebaUmArquivoInvalido() throws Exception {
         this.persister.read(NFNota.class, new File(""));
+    }
+    
+    @Test
+    public void deveParsearXMLDaNota400ModoEstritoComTagAdicionasDeFuturasNormasTecnicas() throws Exception {
+    	
+        String xmlNota = com.fincatto.documentofiscal.nfe400.FabricaDeObjetosFake.getNFNota().toString();
+        
+        xmlNota = mesclaSimulaPropriedadeNovaAdicionadaNormaTecnicaNova(xmlNota);
+        
+        this.persister = new DFPersister(false);
+        
+        final NFNota object = this.persister.read(NFNota.class, xmlNota);
+        
+        Assert.assertNotNull(object);
+    }
+    
+    @Test(expected = ElementException.class)
+    public void deveLancarExcecaoXMLDaNota400SemModoEstritoComTagAdicionasDeFuturasNormasTecnicas() throws Exception {
+    	
+        String xmlNota = com.fincatto.documentofiscal.nfe400.FabricaDeObjetosFake.getNFNota().toString();
+        
+        xmlNota = mesclaSimulaPropriedadeNovaAdicionadaNormaTecnicaNova(xmlNota);
+        
+        this.persister = new DFPersister(true);
+        
+        this.persister.read(NFNota.class, xmlNota);
+        
     }
     
     @Test
@@ -101,5 +131,26 @@ public class DFPersisterTest {
         final File file = new File(new URI(DFPersisterTest.class.getResource("enviaEventoInutilizacao.xml").getFile()).getPath());
         final NFEnviaEventoInutilizacao object = this.persister.read(NFEnviaEventoInutilizacao.class, file);
         Assert.assertNotNull(object);
+    }
+
+	/**
+	 * Metodo auxiliar para simular e mesclar na String a cricao de uma nova
+	 * propriedade/tag no XML, evitando criacao de objetos para representar novas tags
+	 * 
+	 * @param xmlNFNota
+	 * @return xmlNFNota + propriedadeAdicionalNT + xmlNFNota
+	 */
+    private String mesclaSimulaPropriedadeNovaAdicionadaNormaTecnicaNova(String xmlNFNota ) {
+    	
+    	// Exemplo Grupo ZD. Informações do Responsável Técnico - adicionado na NT 2018.005 - Versão 1.20 – Março de 2019
+        // <infRespTec><CNPJ>53113791000122</CNPJ><xContato>Rodrigo de Almeida Sartorio</xContato><email>resp_tecnico_dfe_protheus@totvs.com.br</email><fone>28591383</fone></infRespTec>
+    	
+    	final String fragmentoXMLSimulaNovaPropriedadeNormaTecnica = "<infProgramador><CPF>12345678901</CPF><email>programador@wmixvideo.com</email></infProgramador>";
+    	
+    	if(StringUtils.isNotBlank(xmlNFNota))
+    		xmlNFNota = xmlNFNota.replaceAll("</infNFe>", fragmentoXMLSimulaNovaPropriedadeNormaTecnica+"</infNFe>");
+    	
+    	return xmlNFNota;
+    
     }
 }
