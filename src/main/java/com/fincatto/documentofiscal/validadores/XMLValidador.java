04e2772705141ff5cabca32e7720bec9cc023f92
eldevan <eldevanjr@iservport.com>
2019-06-18 11:12:45 -0300

    Modificações para nova Nota Técnica 2018.002 (Atualizado em 24/08/2018)

--- src/main/java/com/fincatto/documentofiscal/validadores/XMLValidador.java
+++ src/main/java/com/fincatto/documentofiscal/validadores/XMLValidador.java
@@ -57,7 +57,7 @@ public final class XMLValidador {
     }
 
     private static boolean validaCTe(final String xml, final String xsd) throws IOException, SAXException, URISyntaxException {
-        final URL xsdPath = XMLValidador.class.getClassLoader().getResource(String.format("schemas/PL_CTe_300_NT2017.003/%s", xsd));
+        final URL xsdPath = XMLValidador.class.getClassLoader().getResource(String.format("schemas/PL_CTe_300_NT2018.002/%s", xsd));
         final SchemaFactory schemaFactory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
         final Schema schema = schemaFactory.newSchema(new StreamSource(xsdPath.toURI().toString()));
         schema.newValidator().validate(new StreamSource(new StringReader(xml)));
