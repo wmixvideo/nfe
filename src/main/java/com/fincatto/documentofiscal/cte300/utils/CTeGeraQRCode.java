cb2b6ebb2c65ad1462d60d7d426da55c48b3e40a
eldevan <eldevanjr@iservport.com>
2020-01-06 09:55:14 -0300

    Construtor padrão.

--- src/main/java/com/fincatto/documentofiscal/cte300/utils/CTeGeraQRCode.java
+++ src/main/java/com/fincatto/documentofiscal/cte300/utils/CTeGeraQRCode.java
@@ -8,16 +8,14 @@ import com.fincatto.documentofiscal.utils.DFAssinaturaDigital;
 
 public class CTeGeraQRCode {
 
-    private final CTeNota cTeNota;
     private final CTeConfig config;
 
-    public CTeGeraQRCode(final CTeNota nota, final CTeConfig config) {
-        this.cTeNota = nota;
+    public CTeGeraQRCode(CTeConfig config) {
         this.config = config;
     }
 
-    public String getQRCode() throws Exception {
-        String chaveAcesso = this.cTeNota.getCteNotaInfo().getChaveAcesso();
+    public String getQRCode(CTeNota cTeNota) throws Exception {
+        String chaveAcesso = cTeNota.getCteNotaInfo().getChaveAcesso();
         String url = CTAutorizador31.valueOfChaveAcesso(chaveAcesso).getCteQrCode(this.config.getAmbiente());
         final StringBuilder parametros = new StringBuilder();
         parametros.append("chCTe=").append(chaveAcesso).append("&");
