e92a7a54633a6b4b5e19d6db2e6b2429620eb10c
Diego Fincatto <diego@fincatto.com>
2020-01-12 14:49:20 -0300

    Ajustes do PR para melhorias de codigo.

--- src/main/java/com/fincatto/documentofiscal/utils/DFKeyManager.java
+++ src/main/java/com/fincatto/documentofiscal/utils/DFKeyManager.java
@@ -15,36 +15,35 @@ import java.util.Enumeration;
 
 /**
  * Created by Eldevan Nery Junior on 18/10/19.
- *
  */
 public class DFKeyManager implements X509KeyManager, DFLog {
-
+    
     private KeyStore ks;
     private String alias;
     private String password;
-
+    
     public DFKeyManager(DFConfig config) throws KeyStoreException {
         this.ks = config.getCertificadoKeyStore();
         this.alias = getAlias(this.ks);
         this.password = config.getCertificadoSenha();
     }
-
+    
     public String chooseClientAlias(String[] str, Principal[] principal, Socket socket) {
         return alias;
     }
-
+    
     public String chooseServerAlias(String str, Principal[] principal, Socket socket) {
         return alias;
     }
-
+    
     public String[] getClientAliases(String str, Principal[] principal) {
         return new String[]{alias};
     }
-
+    
     public String[] getServerAliases(String str, Principal[] principal) {
         return new String[]{alias};
     }
-
+    
     public X509Certificate[] getCertificateChain(String alias) {
         try {
             Certificate[] certificates = this.ks.getCertificateChain(alias);
@@ -52,23 +51,20 @@ public class DFKeyManager implements X509KeyManager, DFLog {
             System.arraycopy(certificates, 0, x509Certificates, 0, certificates.length);
             return x509Certificates;
         } catch (KeyStoreException e) {
-            this.getLogger().debug("Não foi possível carregar o keystore para o alias:" + alias);
-            this.getLogger().error("Não foi possível carregar o keystore para o alias:" + alias, e);
+            this.getLogger().error("N\u00e3o foi poss\u00edvel carregar o keystore para o alias:" + alias, e);
+            return null;
         }
-
-        return null;
     }
-
+    
     public PrivateKey getPrivateKey(String alias) {
         try {
-            return (PrivateKey) ks.getKey(alias, password == null
-                    ? null : password.toCharArray());
+            return (PrivateKey) ks.getKey(alias, password == null ? null : password.toCharArray());
         } catch (Exception e) {
-            this.getLogger().error("Não foi possível carregar o keystore para o alias:" + alias, e);
+            this.getLogger().error("N\u00e3o foi poss\u00edvel carregar o keystore para o alias:" + alias, e);
+            return null;
         }
-        return null;
     }
-
+    
     private static String getAlias(final KeyStore ks) throws KeyStoreException {
         final Enumeration<String> aliasesEnum = ks.aliases();
         while (aliasesEnum.hasMoreElements()) {
