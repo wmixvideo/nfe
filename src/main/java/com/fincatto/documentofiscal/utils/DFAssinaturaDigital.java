e00482f570069064721ac757da0ee7283ae70661
Diego Fincatto <github@fincatto.com>
2020-11-29 23:10:05 -0300

    Remvido System.out do codigo. Melhorias de legibilidade.

--- src/main/java/com/fincatto/documentofiscal/utils/DFAssinaturaDigital.java
+++ src/main/java/com/fincatto/documentofiscal/utils/DFAssinaturaDigital.java
@@ -1,13 +1,13 @@
 package com.fincatto.documentofiscal.utils;
 
 import com.fincatto.documentofiscal.DFConfig;
+import com.fincatto.documentofiscal.DFLog;
 import org.apache.commons.lang3.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
-import javax.naming.InvalidNameException;
 import javax.naming.ldap.LdapName;
 import javax.xml.crypto.*;
 import javax.xml.crypto.dsig.*;
@@ -25,20 +25,12 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.*;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.Provider;
-import java.security.Signature;
-import java.security.UnrecoverableEntryException;
+import java.security.*;
 import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
+import java.util.*;
 
-public class DFAssinaturaDigital {
+public class DFAssinaturaDigital implements DFLog {
+
     private static final String C14N_TRANSFORM_METHOD = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315";
     private static final String[] ELEMENTOS_ASSINAVEIS = new String[]{"infEvento", "infCanc", "infNFe", "infInut", "infMDFe", "infCte"};
     private final DFConfig config;
@@ -50,11 +42,13 @@ public class DFAssinaturaDigital {
     public boolean isValida(final InputStream xmlStream) throws Exception {
         final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setNamespaceAware(true);
+
         final Document document = dbf.newDocumentBuilder().parse(xmlStream);
         final NodeList nodeList = document.getElementsByTagNameNS(XMLSignature.XMLNS, "Signature");
         if (nodeList.getLength() == 0) {
             throw new IllegalStateException("Nao foi encontrada a assinatura do XML.");
         }
+
         final String providerName = System.getProperty("jsr105Provider", "org.jcp.xml.dsig.internal.dom.XMLDSigRI");
         final XMLSignatureFactory signatureFactory = XMLSignatureFactory.getInstance("DOM", (Provider) Class.forName(providerName).getDeclaredConstructor().newInstance());
         final DOMValidateContext validateContext = new DOMValidateContext(new DFKeySelector(), nodeList.item(0));
@@ -80,30 +74,31 @@ public class DFAssinaturaDigital {
         }
     }
 
-    public void assinarDocumento(Reader xmlReader, Writer xmlAssinado, final String... elementosAssinaveis) throws Exception {
+    public void assinarDocumento(final Reader xmlReader, final Writer xmlAssinado, final String... elementosAssinaveis) throws Exception {
         final KeyStore.PrivateKeyEntry keyEntry = getPrivateKeyEntry();
-        //Adiciona System.out p/ verificação do certificado que assina o documento
-        try {
-            String dn = ((X509Certificate)keyEntry.getCertificate()).getSubjectX500Principal().getName();
-            LdapName ldapDN = null;
-            ldapDN = new LdapName(dn);
-            String commonName = ldapDN.getRdns().stream()
-                    .filter(rdn -> StringUtils.equalsIgnoreCase(rdn.getType(), "CN")).map(val -> val.getValue() + "").findFirst()
-                    .orElse("");
-            System.out.println("CERTIFICADO ASSINANDO(CNPJ):" + commonName );
-        } catch (InvalidNameException e) {
-        }
+
+        final String dn = ((X509Certificate) keyEntry.getCertificate()).getSubjectX500Principal().getName();
+        this.getLogger().debug("DN: {}", dn);
+
+        final String cn = new LdapName(dn).getRdns().stream()
+                .filter(rdn -> StringUtils.equalsIgnoreCase(rdn.getType(), "CN"))
+                .map(val -> String.valueOf(val.getValue()))
+                .findFirst()
+                .orElse("");
+        this.getLogger().debug("CN: {}", cn);
 
 
         final XMLSignatureFactory signatureFactory = XMLSignatureFactory.getInstance("DOM");
         final List<Transform> transforms = new ArrayList<>(2);
         transforms.add(signatureFactory.newTransform(Transform.ENVELOPED, (TransformParameterSpec) null));
         transforms.add(signatureFactory.newTransform(DFAssinaturaDigital.C14N_TRANSFORM_METHOD, (TransformParameterSpec) null));
+
         final KeyInfoFactory keyInfoFactory = signatureFactory.getKeyInfoFactory();
         final X509Data x509Data = keyInfoFactory.newX509Data(Collections.singletonList((X509Certificate) keyEntry.getCertificate()));
         final KeyInfo keyInfo = keyInfoFactory.newKeyInfo(Collections.singletonList(x509Data));
         final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         documentBuilderFactory.setNamespaceAware(true);
+
         final Document document = documentBuilderFactory.newDocumentBuilder().parse(new InputSource(xmlReader));
         for (final String elementoAssinavel : elementosAssinaveis) {
             final NodeList elements = document.getElementsByTagName(elementoAssinavel);
@@ -111,6 +106,7 @@ public class DFAssinaturaDigital {
                 final Element element = (Element) elements.item(i);
                 final String id = element.getAttribute("Id");
                 element.setIdAttribute("Id", true);
+
                 final Reference reference = signatureFactory.newReference("#" + id, signatureFactory.newDigestMethod(DigestMethod.SHA1, null), transforms, null, null);
                 final SignedInfo signedInfo = signatureFactory.newSignedInfo(signatureFactory.newCanonicalizationMethod(CanonicalizationMethod.INCLUSIVE, (C14NMethodParameterSpec) null), signatureFactory.newSignatureMethod(SignatureMethod.RSA_SHA1, null), Collections.singletonList(reference));
                 final XMLSignature signature = signatureFactory.newXMLSignature(signedInfo, keyInfo);
@@ -125,31 +121,28 @@ public class DFAssinaturaDigital {
 
     private KeyStore.PrivateKeyEntry getPrivateKeyEntry() throws KeyStoreException, NoSuchAlgorithmException, UnrecoverableEntryException {
         final KeyStore.PasswordProtection passwordProtection = new KeyStore.PasswordProtection(this.config.getCertificadoSenha().toCharArray());
-        //verifica se há um Alias configurado
-        if(StringUtils.isNotEmpty(config.getCertificadoAlias())){
-            final String certificateAlias = config.getCertificadoAlias() != null ? config.getCertificadoAlias()
-                    : config.getCertificadoKeyStore().aliases().nextElement();
-            return (KeyStore.PrivateKeyEntry) config.getCertificadoKeyStore().getEntry(certificateAlias, passwordProtection);
-        }else{
-            KeyStore ks = config.getCertificadoKeyStore();
-            for (Enumeration<String> e = ks.aliases(); e.hasMoreElements();) {
-                String alias = e.nextElement();
+        if (StringUtils.isNotBlank(config.getCertificadoAlias())) {
+            this.getLogger().debug("Usando alias informado: '{}'", config.getCertificadoAlias());
+            return (KeyStore.PrivateKeyEntry) config.getCertificadoKeyStore().getEntry(config.getCertificadoAlias(), passwordProtection);
+        } else {
+            final KeyStore ks = config.getCertificadoKeyStore();
+            for (Enumeration<String> e = ks.aliases(); e.hasMoreElements(); ) {
+                final String alias = e.nextElement();
                 if (ks.isKeyEntry(alias)) {
+                    this.getLogger().debug("Usando alias descoberto: '{}'", alias);
                     return (KeyStore.PrivateKeyEntry) ks.getEntry(alias, passwordProtection);
                 }
             }
-            throw new RuntimeException("Não foi possível encontrar a chave privada do certificado");
+            throw new KeyStoreException("N\u00E3o foi poss\u00EDvel encontrar a chave privada do certificado!");
         }
     }
 
-    public String assinarString(String _string) throws Exception {
-        byte[] buffer = _string.getBytes();
-        Signature signatureProvider = Signature.getInstance("SHA1withRSA");
+    public String assinarString(final String string) throws Exception {
+        final byte[] buffer = string.getBytes();
+        final Signature signatureProvider = Signature.getInstance("SHA1withRSA");
         signatureProvider.initSign(getPrivateKeyEntry().getPrivateKey());
         signatureProvider.update(buffer, 0, buffer.length);
-        byte[] signature = signatureProvider.sign();
-        System.out.println(getPrivateKeyEntry().getPrivateKey().getFormat());
-        return Base64.getEncoder().encodeToString(signature);
+        return Base64.getEncoder().encodeToString(signatureProvider.sign());
     }
 
     static class DFKeySelector extends KeySelector {
