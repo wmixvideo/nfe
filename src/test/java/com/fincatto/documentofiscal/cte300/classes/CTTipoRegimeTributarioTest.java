767998d7e0ed72db5f7e9c90a4e1088a115703bb
Maurício Dantas <mdantas@imagemfilmes.com.br>
2022-09-21 10:15:53 -0300

    Inclusão de atributo tipo regime tributário em identificação de emitemente

---
+++ src/test/java/com/fincatto/documentofiscal/cte300/classes/CTTipoRegimeTributarioTest.java
@@ -0,0 +1,18 @@
+package com.fincatto.documentofiscal.cte300.classes;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CTTipoRegimeTributarioTest {
+
+
+    @Test
+    public void deveRepresentarOCodigoCorretamente() {
+        Assert.assertNull(CTTipoRegimeTributario.valueOfCodigo(null));
+        Assert.assertEquals("1", CTTipoRegimeTributario.SIMPLES_NACIONAL.getCodigo());
+        Assert.assertEquals("2", CTTipoRegimeTributario.SIMPLES_NACIONAL_EXCESSO_SUBLIME_RECEITA_BRUTA.getCodigo());
+        Assert.assertEquals("3", CTTipoRegimeTributario.REGIME_NORMAL.getCodigo());
+    }
+
+}
