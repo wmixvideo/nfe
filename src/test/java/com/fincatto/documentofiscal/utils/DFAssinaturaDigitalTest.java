2dcc4afc0bdf33d4ac2ef0af0b572a5f5b65a5f0
Diego Fincatto <diego@fincatto.com>
2022-11-01 10:11:18 -0300

    Migrado para Java11 (#845)
    * Testando migracao para java 11.
    
    * Adicionado dependencia do apache santuario para validacao de assinaturas.
    
    * Adicionado dependencia do apache santuario para validacao de assinaturas.

--- src/test/java/com/fincatto/documentofiscal/utils/DFAssinaturaDigitalTest.java
+++ src/test/java/com/fincatto/documentofiscal/utils/DFAssinaturaDigitalTest.java
@@ -5,15 +5,15 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.file.Files;
 
 public class DFAssinaturaDigitalTest {
 
     @Test
     @Ignore
     public void isValida() throws Exception {
-        try (InputStream inputStream = new FileInputStream(new File("/tmp/nota_a_validar.xml"))) {
+        try (InputStream inputStream = Files.newInputStream(new File("/tmp/42221012672380000190550010001580701847885151.xml").toPath())) {
             Assert.assertTrue(DFAssinaturaDigital.isValida(inputStream));
         }
     }
