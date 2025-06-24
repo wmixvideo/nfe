5efd2d3b1c0e07e2cb770eed4d37b0c3ac8019cb
tecnosofti <tecnosofti@amdryzen>
2025-06-23 22:44:23 -0300

    Testes unitario NFNotaInfoImpostoTributacaoIBSCBS

---
+++ src/test/java/com/fincatto/documentofiscal/nfe400/classes/NFNotaInfoImpostoTributacaoIBSCBSTest.java
@@ -0,0 +1,26 @@
+package com.fincatto.documentofiscal.nfe400.classes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NFNotaInfoImpostoTributacaoIBSCBSTest {
+
+	@Test
+	public void deveRepresentarCodigoCorretamente() {
+		Assert.assertEquals("000001", NFNotaInfoImpostoTributacaoIBSCBS.SITUACOES_TRIBUTADAS_INTEGRALMENTE_PELO_IBS_E_CBS.getcClassTrib());
+		Assert.assertEquals("000002", NFNotaInfoImpostoTributacaoIBSCBS.EXPLORACAO_DE_VIA.getcClassTrib());
+		Assert.assertEquals("000003", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_311.getcClassTrib());
+		Assert.assertEquals("000004", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_312.getcClassTrib());
+		Assert.assertEquals("010001", NFNotaInfoImpostoTributacaoIBSCBS.TRIBUTACAO_COM_ALIQUOTAS_UNIFORMES_OPERACOES_FGTS.getcClassTrib());
+	}
+
+	@Test
+    public void deveRepresentarDescricaoCorretamente() {
+		Assert.assertEquals("Situações tributadas integralmente pelo IBS e CBS", NFNotaInfoImpostoTributacaoIBSCBS.SITUACOES_TRIBUTADAS_INTEGRALMENTE_PELO_IBS_E_CBS.getxClassTrib());
+		Assert.assertEquals("Exploração de via", NFNotaInfoImpostoTributacaoIBSCBS.EXPLORACAO_DE_VIA.getxClassTrib());
+		Assert.assertEquals("Regime automotivo - projetos incentivados (art. 311)", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_311.getxClassTrib());
+		Assert.assertEquals("Regime automotivo - projetos incentivados (art. 312)", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_312.getxClassTrib());
+		Assert.assertEquals("Tributação com alíquotas uniformes - operações do FGTS", NFNotaInfoImpostoTributacaoIBSCBS.TRIBUTACAO_COM_ALIQUOTAS_UNIFORMES_OPERACOES_FGTS.getxClassTrib());
+    }
+	
+}
