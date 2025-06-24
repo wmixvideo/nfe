5a9f1c52ac30cce985291ffadac8f9ea20580276
tecnosofti <tecnosofti@amdryzen>
2025-06-23 22:47:39 -0300

    Testes unitario NFNotaInfoImpostoTributacaoIBSCBS

--- src/test/java/com/fincatto/documentofiscal/nfe400/classes/NFNotaInfoImpostoTributacaoIBSCBSTest.java
+++ src/test/java/com/fincatto/documentofiscal/nfe400/classes/NFNotaInfoImpostoTributacaoIBSCBSTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class NFNotaInfoImpostoTributacaoIBSCBSTest {
 
 	@Test
-	public void deveRepresentarCodigoCorretamente() {
+	public void deveRepresentarCodigoClassTribCorretamente() {
 		Assert.assertEquals("000001", NFNotaInfoImpostoTributacaoIBSCBS.SITUACOES_TRIBUTADAS_INTEGRALMENTE_PELO_IBS_E_CBS.getcClassTrib());
 		Assert.assertEquals("000002", NFNotaInfoImpostoTributacaoIBSCBS.EXPLORACAO_DE_VIA.getcClassTrib());
 		Assert.assertEquals("000003", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_311.getcClassTrib());
@@ -15,7 +15,7 @@ public class NFNotaInfoImpostoTributacaoIBSCBSTest {
 	}
 
 	@Test
-    public void deveRepresentarDescricaoCorretamente() {
+    public void deveRepresentarDescricaoClassTribCorretamente() {
 		Assert.assertEquals("Situações tributadas integralmente pelo IBS e CBS", NFNotaInfoImpostoTributacaoIBSCBS.SITUACOES_TRIBUTADAS_INTEGRALMENTE_PELO_IBS_E_CBS.getxClassTrib());
 		Assert.assertEquals("Exploração de via", NFNotaInfoImpostoTributacaoIBSCBS.EXPLORACAO_DE_VIA.getxClassTrib());
 		Assert.assertEquals("Regime automotivo - projetos incentivados (art. 311)", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_311.getxClassTrib());
