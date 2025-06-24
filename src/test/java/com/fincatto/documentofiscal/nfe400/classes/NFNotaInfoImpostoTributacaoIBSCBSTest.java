d2279b1fbec75cfc11352ca8323d07ef123e7400
tecnosofti <tecnosofti@amdryzen>
2025-06-23 23:03:36 -0300

    Testes unitario NFNotaInfoImpostoTributacaoIBSCBS

--- src/test/java/com/fincatto/documentofiscal/nfe400/classes/NFNotaInfoImpostoTributacaoIBSCBSTest.java
+++ src/test/java/com/fincatto/documentofiscal/nfe400/classes/NFNotaInfoImpostoTributacaoIBSCBSTest.java
@@ -6,21 +6,39 @@ import org.junit.Test;
 public class NFNotaInfoImpostoTributacaoIBSCBSTest {
 
 	@Test
+	public void deveRepresentarCodigoCstISCBSCorretamente() {
+		Assert.assertEquals("000", NFNotaInfoImpostoTributacaoIBSCBS.SITUACOES_TRIBUTADAS_INTEGRALMENTE_PELO_IBS_E_CBS.getcCstIBSCBS());
+		Assert.assertEquals("000", NFNotaInfoImpostoTributacaoIBSCBS.EXPLORACAO_DE_VIA.getcCstIBSCBS());
+		Assert.assertEquals("000", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_311.getcCstIBSCBS());
+		Assert.assertEquals("000", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_312.getcCstIBSCBS());
+		Assert.assertEquals("010", NFNotaInfoImpostoTributacaoIBSCBS.OPERACOES_FGTS_NAO_REALIZADAS_PELA_CAIXA_ECONOMICA_FEDERAL.getcCstIBSCBS());
+	}
+	
+	@Test
 	public void deveRepresentarCodigoClassTribCorretamente() {
 		Assert.assertEquals("000001", NFNotaInfoImpostoTributacaoIBSCBS.SITUACOES_TRIBUTADAS_INTEGRALMENTE_PELO_IBS_E_CBS.getcClassTrib());
 		Assert.assertEquals("000002", NFNotaInfoImpostoTributacaoIBSCBS.EXPLORACAO_DE_VIA.getcClassTrib());
 		Assert.assertEquals("000003", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_311.getcClassTrib());
 		Assert.assertEquals("000004", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_312.getcClassTrib());
-		Assert.assertEquals("010001", NFNotaInfoImpostoTributacaoIBSCBS.TRIBUTACAO_COM_ALIQUOTAS_UNIFORMES_OPERACOES_FGTS.getcClassTrib());
+		Assert.assertEquals("010001", NFNotaInfoImpostoTributacaoIBSCBS.OPERACOES_FGTS_NAO_REALIZADAS_PELA_CAIXA_ECONOMICA_FEDERAL.getcClassTrib());
 	}
 
 	@Test
-    public void deveRepresentarDescricaoClassTribCorretamente() {
+	public void deveRepresentarDescricaoClassTribCorretamente() {
 		Assert.assertEquals("Situações tributadas integralmente pelo IBS e CBS", NFNotaInfoImpostoTributacaoIBSCBS.SITUACOES_TRIBUTADAS_INTEGRALMENTE_PELO_IBS_E_CBS.getxClassTrib());
 		Assert.assertEquals("Exploração de via", NFNotaInfoImpostoTributacaoIBSCBS.EXPLORACAO_DE_VIA.getxClassTrib());
 		Assert.assertEquals("Regime automotivo - projetos incentivados (art. 311)", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_311.getxClassTrib());
 		Assert.assertEquals("Regime automotivo - projetos incentivados (art. 312)", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_312.getxClassTrib());
-		Assert.assertEquals("Tributação com alíquotas uniformes - operações do FGTS", NFNotaInfoImpostoTributacaoIBSCBS.TRIBUTACAO_COM_ALIQUOTAS_UNIFORMES_OPERACOES_FGTS.getxClassTrib());
+		Assert.assertEquals("Operações do FGTS não realizadas pela Caixa Econômica Federal", NFNotaInfoImpostoTributacaoIBSCBS.OPERACOES_FGTS_NAO_REALIZADAS_PELA_CAIXA_ECONOMICA_FEDERAL.getxClassTrib());
+	}
+	
+	@Test
+    public void deveRepresentarDescricaoCstIBSCBSCorretamente() {
+		Assert.assertEquals("Tributação integral", NFNotaInfoImpostoTributacaoIBSCBS.SITUACOES_TRIBUTADAS_INTEGRALMENTE_PELO_IBS_E_CBS.getxCstIBSCBS());
+		Assert.assertEquals("Tributação integral", NFNotaInfoImpostoTributacaoIBSCBS.EXPLORACAO_DE_VIA.getxCstIBSCBS());
+		Assert.assertEquals("Tributação integral", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_311.getxCstIBSCBS());
+		Assert.assertEquals("Tributação integral", NFNotaInfoImpostoTributacaoIBSCBS.REGIME_AUTOMOTIVO_PROJETOS_INCENTIVADOS_ART_312.getxCstIBSCBS());
+		Assert.assertEquals("Tributação com alíquotas uniformes - operações do FGTS", NFNotaInfoImpostoTributacaoIBSCBS.OPERACOES_FGTS_NAO_REALIZADAS_PELA_CAIXA_ECONOMICA_FEDERAL.getxCstIBSCBS());
     }
 	
 }
