75c3d95fe14352dae949c1221d656c32d0a63565
Diego Fincatto <diego@fincatto.com>
2025-07-17 20:55:04 -0300

    Ajustado nomes enums IBS CBS. Seguindo o padrao existente em NFNotaInfoImpostoTributacaoICMS, para facilitar a leitura.

--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/NFNotaInfoImpostoTributacaoIBSCBSClassTrib.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/NFNotaInfoImpostoTributacaoIBSCBSClassTrib.java
@@ -1,87 +1,85 @@
 package com.fincatto.documentofiscal.nfe400.classes;
 
-import static com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoImpostoTributacaoIBSCBS._000;
-import static com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoImpostoTributacaoIBSCBS._010;
-import static com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoImpostoTributacaoIBSCBS._011;
-import static com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoImpostoTributacaoIBSCBS._200;
-import static com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoImpostoTributacaoIBSCBS._830;
+import static com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoImpostoTributacaoIBSCBS.TRIBUTACAO_INTEGRAL;
+import static com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoImpostoTributacaoIBSCBS.TRIBUTACAO_ALIQUOTAS_UNIFORMES;
+import static com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoImpostoTributacaoIBSCBS.TRIBUTACAO_ALIQUOTAS_UNIFORMES_REDUZIDAS;
+import static com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoImpostoTributacaoIBSCBS.ALIQUOTA_REDUZIDA;
+import static com.fincatto.documentofiscal.nfe400.classes.NFNotaInfoImpostoTributacaoIBSCBS.EXCLUSAO_BASE_CALCULO;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 /**
- * Este enum é uma proposta (ainda não utilizada, e pode ser excluída), de
- * acordo com a tabela tabela CST_cClassTribPublicacao_20250618. Ainda não está
- * com todos os códigos da tabela.
- *
+ * Este enum é uma proposta (ainda não utilizada, e pode ser excluída), de acordo com a tabela tabela CST_cClassTribPublicacao_20250618.
+ * Ainda não está com todos os códigos da tabela.
  * @author Edivaldo Merlo Stens
  */
 public enum NFNotaInfoImpostoTributacaoIBSCBSClassTrib {
 
-  _000001("000001", _000, "Situações tributadas integralmente pelo IBS e CBS",
+  SITUACOES_TRIBUTADAS_INTEGRALMENTE("000001", TRIBUTACAO_INTEGRAL, "Situações tributadas integralmente pelo IBS e CBS",
       C.P_RED_IBS_N, C.P_RED_CBS_N, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _000002("000002", _000, "Exploração de via",
+  EXPLORACAO_DE_VIA("000002", TRIBUTACAO_INTEGRAL, "Exploração de via",
       C.P_RED_IBS_N, C.P_RED_CBS_N, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _000003("000003", _000, "Regime automotivo - projetos incentivados (art. 311)",
+  REGIME_AUTOMOTIVO_ART311("000003", TRIBUTACAO_INTEGRAL, "Regime automotivo - projetos incentivados (art. 311)",
       C.P_RED_IBS_N, C.P_RED_CBS_N, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_1, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _000004("000004", _000, "Regime automotivo - projetos incentivados (art. 312)",
+  REGIME_AUTOMOTIVO_ART312("000004", TRIBUTACAO_INTEGRAL, "Regime automotivo - projetos incentivados (art. 312)",
       C.P_RED_IBS_N, C.P_RED_CBS_N, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_1, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _010001("010001", _010, "Operações do FGTS não realizadas pela Caixa Econômica Federal",
+  OPERACOES_FGTS_NAO_REALIZADAS_CEF("010001", TRIBUTACAO_ALIQUOTAS_UNIFORMES, "Operações do FGTS não realizadas pela Caixa Econômica Federal",
       C.P_RED_IBS_N, C.P_RED_CBS_N, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _010002("010002", _010, "Operações do serviço financeiro",
+  OPERACOES_SERVICO_FINANCEIRO("010002", TRIBUTACAO_ALIQUOTAS_UNIFORMES, "Operações do serviço financeiro",
       C.P_RED_IBS_N, C.P_RED_CBS_N, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _011001("011001", _011, "Planos de assistência funerária.",
+  PLANO_ASSISTENCIA_FUNERARIA("011001", TRIBUTACAO_ALIQUOTAS_UNIFORMES_REDUZIDAS, "Planos de assistência funerária.",
       C.P_RED_IBS_60, C.P_RED_CBS_60, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _011002("011002", _011, "Planos de assistência à saúde",
+  PLANO_ASSISTENCIA_SAUDE("011002", TRIBUTACAO_ALIQUOTAS_UNIFORMES_REDUZIDAS, "Planos de assistência à saúde",
       C.P_RED_IBS_60, C.P_RED_CBS_60, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _011003("011003", _011, "Intermediação de planos de assistência à saúde",
+  INTERMEDIACAO_PLANO_ASSISTENCIA_SAUDE("011003", TRIBUTACAO_ALIQUOTAS_UNIFORMES_REDUZIDAS, "Intermediação de planos de assistência à saúde",
       C.P_RED_IBS_60, C.P_RED_CBS_60, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _011004("011004", _011, "Concursos e prognósticos",
+  CONCURSOS_PROGNOSTICOS("011004", TRIBUTACAO_ALIQUOTAS_UNIFORMES_REDUZIDAS, "Concursos e prognósticos",
       C.P_RED_IBS_N, C.P_RED_CBS_N, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _011005("011005", _011, "Planos de assistência à saúde de animais domésticos",
+  PLANOS_ASSISTENCIA_SAUDE_ANIMAIS_DOMESTICOS("011005", TRIBUTACAO_ALIQUOTAS_UNIFORMES_REDUZIDAS, "Planos de assistência à saúde de animais domésticos",
       C.P_RED_IBS_30, C.P_RED_CBS_30, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _200001("200001", _200, "Aquisições realizadas entre empresas autorizadas a operar em zonas de processamento de exportação",
+  AQUISICOES_ENTRE_EMPRESAS_AUTORIZADAS_OPERAR_ZONAS_PROCESSAMENTO_EXPORTACAO("200001", ALIQUOTA_REDUZIDA, "Aquisições realizadas entre empresas autorizadas a operar em zonas de processamento de exportação",
       C.P_RED_IBS_100, C.P_RED_CBS_100, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _200002("200002", _200, "Fornecimento ou importação para produtor rural não contribuinte ou TAC",
+  FORNECIMENTO_IMPORTACAO_PRODUTOR_RURAL_NAO_CONTRIBUINTE_TAC("200002", ALIQUOTA_REDUZIDA, "Fornecimento ou importação para produtor rural não contribuinte ou TAC",
       C.P_RED_IBS_100, C.P_RED_CBS_100, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _200003("200003", _200, "Vendas de produtos destinados à alimentação humana (Anexo I)",
+  VENDAS_PRODUTOS_ALIMENTACAO_HUMANA("200003", ALIQUOTA_REDUZIDA, "Vendas de produtos destinados à alimentação humana (Anexo I)",
       C.P_RED_IBS_100, C.P_RED_CBS_100, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _200004("200004", _200, "Venda de dispositivos médicos com a especificação das respectivas classificações da NCM/SH previstas no Anexo XII da Lei Complementar nº 214, de 2025, observado o art. 144 da Lei Complementar nº 214, de 2025.",
+  VENDA_DISPOSITIVOS_MEDICOS_ANEXO_XII("200004", ALIQUOTA_REDUZIDA, "Venda de dispositivos médicos com a especificação das respectivas classificações da NCM/SH previstas no Anexo XII da Lei Complementar nº 214, de 2025, observado o art. 144 da Lei Complementar nº 214, de 2025.",
       C.P_RED_IBS_100, C.P_RED_CBS_100, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _200005("200005", _200, "Venda de dispositivos médicos adquiridos por órgãos da administração pública (Anexo IV).",
+  VENDA_DISPOSITIVOS_MEDICOS_ANEXO_IV("200005", ALIQUOTA_REDUZIDA, "Venda de dispositivos médicos adquiridos por órgãos da administração pública (Anexo IV).",
       C.P_RED_IBS_100, C.P_RED_CBS_100, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _200006("200006", _200, "Situação de emergência de saúde pública reconhecida pelo Poder público (Anexo XII).",
+  SITUACAO_EMERGENCIA_SAUDE_PUBLICA_ANEXO_XII("200006", ALIQUOTA_REDUZIDA, "Situação de emergência de saúde pública reconhecida pelo Poder público (Anexo XII).",
       C.P_RED_IBS_100, C.P_RED_CBS_100, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _200007("200007", _200, "Fornecimento dos dispositivos de acessibilidade próprios para pessoas com deficiência (Anexo XIII).",
+  FORNECIMENTO_DISPOSITIVOS_ACESSIBILIDADE_ANEXO_XIII("200007", ALIQUOTA_REDUZIDA, "Fornecimento dos dispositivos de acessibilidade próprios para pessoas com deficiência (Anexo XIII).",
       C.P_RED_IBS_100, C.P_RED_CBS_100, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _200008("200008", _200, "Fornecimento dos dispositivos de acessibilidade próprios para pessoas com deficiência adquiridos por órgãos da administração pública (Anexo V).",
+  FORNECIMENTO_DISPOSITIVOS_ACESSIBILIDADE_ANEXO_V("200008", ALIQUOTA_REDUZIDA, "Fornecimento dos dispositivos de acessibilidade próprios para pessoas com deficiência adquiridos por órgãos da administração pública (Anexo V).",
       C.P_RED_IBS_100, C.P_RED_CBS_100, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _200009("200009", _200, "Fornecimento de medicamentos (Anexo XIV).",
+  FORNECIMENTO_MEDICAMENTOS_ANEXO_XIV("200009", ALIQUOTA_REDUZIDA, "Fornecimento de medicamentos (Anexo XIV).",
       C.P_RED_IBS_100, C.P_RED_CBS_100, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
-  _200010("200010", _200, "Fornecimento dos medicamentos registrados na Anvisa, adquiridos por órgãos da administração pública.",
+  FORNECIMENTO_MEDICAMENTOS_ADMINISTRACAO_PUBLICA("200010", ALIQUOTA_REDUZIDA, "Fornecimento dos medicamentos registrados na Anvisa, adquiridos por órgãos da administração pública.",
       C.P_RED_IBS_100, C.P_RED_CBS_100, C.IND_RED_BC_N, C.IND_GTRIB_REG_0, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null),
   // falta implementar o restante de acordo com a tabela CST_cClassTribPublicacao_20250618
-  _830001("830001", _830, "Documento com exclusão da BC da CBS e do IBS de energia elétrica fornecida pela distribuidora à UC",
+  DOCUMENTO_EXCLUSAO_BC_ENERGIA_DISTRIBUIDORA_UC("830001", EXCLUSAO_BASE_CALCULO, "Documento com exclusão da BC da CBS e do IBS de energia elétrica fornecida pela distribuidora à UC",
       C.P_RED_IBS_N, C.P_RED_CBS_N, C.IND_RED_BC_N, C.IND_GTRIB_REG_N, C.IND_CRED_PRES_N, C.IND_MONO_N, C.IND_MONO_RETEN_N,
       C.IND_MONO_RET_N, C.IND_MONO_DIF_N, null, null);
 
