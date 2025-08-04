a1b7c85105c5f3389b8722ee56c7d7cd93d29a44
tecnosofti <tecnosofti@amdryzen>
2025-08-04 12:49:35 -0300

    NT_2025.002_v1.20_RTC_NF-e_IBS_CBS_IS

--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/NFCredito.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/NFCredito.java
@@ -3,7 +3,8 @@ package com.fincatto.documentofiscal.nfe400.classes;
 public enum NFCredito {
 
   MULTA_JUROS("01", "Multa e juros"),
-  APROPRIACAO_CREDITO_PRESUMIDO_IBS_SALDO_DEVEDOR_ZFM("02", "Apropriação de crédito presumido de IBS sobre o saldo devedor na ZFM (art. 450, § 1º, LC 214/25)");
+  APROPRIACAO_CREDITO_PRESUMIDO_IBS_SALDO_DEVEDOR_ZFM("02", "Apropriação de crédito presumido de IBS sobre o saldo devedor na ZFM (art. 450, § 1º, LC 214/25)"),
+  RETORNO("03", "Retorno");
 
   private final String codigo;
   private final String descricao;
--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoIBSCBS.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoIBSCBS.java
@@ -11,73 +11,117 @@ import org.simpleframework.xml.Element;
  */
 public class NFNotaInfoItemImpostoIBSCBS extends DFBase {
 
-  private static final long serialVersionUID = -366528394939416671L;
+	private static final long serialVersionUID = -366528394939416671L;
 
-  @Element(name = "CST", required = true)
-  private NFNotaInfoImpostoTributacaoIBSCBS cst; // UB13
+	@Element(name = "CST", required = true)
+	private NFNotaInfoImpostoTributacaoIBSCBS cst; // UB13
 
-  @Element(required = true)
-  private String cClassTrib; // UB14
+	@Element(required = true)
+	private String cClassTrib; // UB14
 
-  // UB14a -x- Sequencia XML
-  @Element(required = false)
-  private NFNotaInfoItemImpostoIBSCBSTIBS gIBSCBS; // UB15
+	// UB14a -x- Sequencia XML
+	@Element(required = false)
+	private NFNotaInfoItemImpostoIBSCBSTIBS gIBSCBS; // UB15
 
-  @Element(required = false)
-    private NFNotaInfoItemImpostoIBSCBSMonofasia gIBSCBSMono; // UB84
+	@Element(required = false)
+	private NFNotaInfoItemImpostoIBSCBSMonofasia gIBSCBSMono; // UB84
 
-  @Element(required = false)
-  private NFNotaInfoItemImpostoIBSCBSTransfCred gTransfCred; // UB106
+	@Element(required = false)
+	private NFNotaInfoItemImpostoIBSCBSMonofasia gMonoPadrao; // UB84a
 
-  @Element(required = false)
-  private NFNotaInfoItemImpostoIBSCBSCredPresIBSZFM gCredPresIBSZFM; // UB109
+	@Element(required = false)
+	private NFNotaInfoItemImpostoIBSCBSMonofasia gMonoReten; // UB90
 
-  public NFNotaInfoImpostoTributacaoIBSCBS getCst() {
-    return cst;
-  }
+	@Element(required = false)
+	private NFNotaInfoItemImpostoIBSCBSMonofasia gMonoRet; // UB94
 
-  public void setCst(NFNotaInfoImpostoTributacaoIBSCBS cst) {
-    this.cst = cst;
-  }
+	@Element(required = false)
+	private NFNotaInfoItemImpostoIBSCBSMonofasia gMonoDif; // UB99
 
-  public String getcClassTrib() {
-    return cClassTrib;
-  }
+	@Element(required = false)
+	private NFNotaInfoItemImpostoIBSCBSTransfCred gTransfCred; // UB106
 
-  public void setcClassTrib(String cClassTrib) {
-    this.cClassTrib = cClassTrib;
-  }
+	@Element(required = false)
+	private NFNotaInfoItemImpostoIBSCBSCredPresIBSZFM gCredPresIBSZFM; // UB109
 
-  public NFNotaInfoItemImpostoIBSCBSTIBS getGIBSCBS() {
-    return gIBSCBS;
-  }
+	public NFNotaInfoImpostoTributacaoIBSCBS getCst() {
+		return cst;
+	}
 
-  public void setGIBSCBS(NFNotaInfoItemImpostoIBSCBSTIBS gIBSCBS) {
-    this.gIBSCBS = gIBSCBS;
-  }
+	public void setCst(NFNotaInfoImpostoTributacaoIBSCBS cst) {
+		this.cst = cst;
+	}
 
-  public NFNotaInfoItemImpostoIBSCBSMonofasia getGIBSCBSMono() {
-    return gIBSCBSMono;
-  }
+	public String getcClassTrib() {
+		return cClassTrib;
+	}
 
-  public void setGIBSCBSMono(NFNotaInfoItemImpostoIBSCBSMonofasia gIBSCBSMono) {
-    this.gIBSCBSMono = gIBSCBSMono;
-  }
+	public void setcClassTrib(String cClassTrib) {
+		this.cClassTrib = cClassTrib;
+	}
 
-  public NFNotaInfoItemImpostoIBSCBSTransfCred getGTransfCred() {
-    return gTransfCred;
-  }
+	public NFNotaInfoItemImpostoIBSCBSTIBS getGIBSCBS() {
+		return gIBSCBS;
+	}
 
-  public void setGTransfCred(NFNotaInfoItemImpostoIBSCBSTransfCred gTransfCred) {
-    this.gTransfCred = gTransfCred;
-  }
+	public void setGIBSCBS(NFNotaInfoItemImpostoIBSCBSTIBS gIBSCBS) {
+		this.gIBSCBS = gIBSCBS;
+	}
 
-  public NFNotaInfoItemImpostoIBSCBSCredPresIBSZFM getGCredPresIBSZFM() {
-    return gCredPresIBSZFM;
-  }
+	public NFNotaInfoItemImpostoIBSCBSMonofasia getGIBSCBSMono() {
+		return gIBSCBSMono;
+	}
 
-  public void setGCredPresIBSZFM(NFNotaInfoItemImpostoIBSCBSCredPresIBSZFM gCredPresIBSZFM) {
-    this.gCredPresIBSZFM = gCredPresIBSZFM;
-  }
+	public void setGIBSCBSMono(NFNotaInfoItemImpostoIBSCBSMonofasia gIBSCBSMono) {
+		this.gIBSCBSMono = gIBSCBSMono;
+	}
+
+	public NFNotaInfoItemImpostoIBSCBSMonofasia getGMonoPadrao() {
+		return gMonoPadrao;
+	}
+
+	public void setGMonoPadrao(NFNotaInfoItemImpostoIBSCBSMonofasia gMonoPadrao) {
+		this.gMonoPadrao = gMonoPadrao;
+	}
+
+	public NFNotaInfoItemImpostoIBSCBSMonofasia getGMonoReten() {
+		return gMonoReten;
+	}
+
+	public void setGMonoReten(NFNotaInfoItemImpostoIBSCBSMonofasia gMonoReten) {
+		this.gMonoReten = gMonoReten;
+	}
+
+	public NFNotaInfoItemImpostoIBSCBSMonofasia getGMonoRet() {
+		return gMonoRet;
+	}
+
+	public void setGMonoRet(NFNotaInfoItemImpostoIBSCBSMonofasia gMonoRet) {
+		this.gMonoRet = gMonoRet;
+	}
+
+	public NFNotaInfoItemImpostoIBSCBSMonofasia getGMonoDif() {
+		return gMonoDif;
+	}
+
+	public void setGMonoDif(NFNotaInfoItemImpostoIBSCBSMonofasia gMonoDif) {
+		this.gMonoDif = gMonoDif;
+	}
+
+	public NFNotaInfoItemImpostoIBSCBSTransfCred getGTransfCred() {
+		return gTransfCred;
+	}
+
+	public void setGTransfCred(NFNotaInfoItemImpostoIBSCBSTransfCred gTransfCred) {
+		this.gTransfCred = gTransfCred;
+	}
+
+	public NFNotaInfoItemImpostoIBSCBSCredPresIBSZFM getGCredPresIBSZFM() {
+		return gCredPresIBSZFM;
+	}
+
+	public void setGCredPresIBSZFM(NFNotaInfoItemImpostoIBSCBSCredPresIBSZFM gCredPresIBSZFM) {
+		this.gCredPresIBSZFM = gCredPresIBSZFM;
+	}
 
 }
