c75093a43b05a5f5fe08f1bf8f900085c6d7e1ba
Ommega Data <proomegadata@gmail.com>
2025-09-26 15:59:54 -0300

    Alterado a classe NFNotaInfoItemImpostoIBSCBS removido os campos do gMono e adicionado dentro da classe NFNotaInfoItemImpostoIBSCBSMonofasia para seguir o layout da NT_2025.002_v1.20_RTC_NF-e_IBS_CBS_IS, criada as classes no padrão do projeto e criado os testes

--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoIBSCBS.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/nota/NFNotaInfoItemImpostoIBSCBS.java
@@ -11,73 +11,73 @@ import org.simpleframework.xml.Element;
  */
 public class NFNotaInfoItemImpostoIBSCBS extends DFBase {
 
-  private static final long serialVersionUID = -366528394939416671L;
+    private static final long serialVersionUID = -366528394939416671L;
 
-  @Element(name = "CST", required = true)
-  private NFNotaInfoImpostoTributacaoIBSCBS cst; // UB13
+    @Element(name = "CST", required = true)
+    private NFNotaInfoImpostoTributacaoIBSCBS cst; // UB13
 
-  @Element(required = true)
-  private String cClassTrib; // UB14
+    @Element(required = true)
+    private String cClassTrib; // UB14
 
-  // UB14a -x- Sequencia XML
-  @Element(required = false)
-  private NFNotaInfoItemImpostoIBSCBSTIBS gIBSCBS; // UB15
+    // UB14a -x- Sequencia XML
+    @Element(required = false)
+    private NFNotaInfoItemImpostoIBSCBSTIBS gIBSCBS; // UB15
 
-  @Element(required = false)
+    @Element(required = false)
     private NFNotaInfoItemImpostoIBSCBSMonofasia gIBSCBSMono; // UB84
 
-  @Element(required = false)
-  private NFNotaInfoItemImpostoIBSCBSTransfCred gTransfCred; // UB106
+    @Element(required = false)
+    private NFNotaInfoItemImpostoIBSCBSTransfCred gTransfCred; // UB106
 
-  @Element(required = false)
-  private NFNotaInfoItemImpostoIBSCBSCredPresIBSZFM gCredPresIBSZFM; // UB109
+    @Element(required = false)
+    private NFNotaInfoItemImpostoIBSCBSCredPresIBSZFM gCredPresIBSZFM; // UB109
 
-  public NFNotaInfoImpostoTributacaoIBSCBS getCst() {
-    return cst;
-  }
+    public NFNotaInfoImpostoTributacaoIBSCBS getCst() {
+        return cst;
+    }
 
-  public void setCst(NFNotaInfoImpostoTributacaoIBSCBS cst) {
-    this.cst = cst;
-  }
+    public void setCst(NFNotaInfoImpostoTributacaoIBSCBS cst) {
+        this.cst = cst;
+    }
 
-  public String getcClassTrib() {
-    return cClassTrib;
-  }
+    public String getcClassTrib() {
+        return cClassTrib;
+    }
 
-  public void setcClassTrib(String cClassTrib) {
-    this.cClassTrib = cClassTrib;
-  }
+    public void setcClassTrib(String cClassTrib) {
+        this.cClassTrib = cClassTrib;
+    }
 
-  public NFNotaInfoItemImpostoIBSCBSTIBS getGIBSCBS() {
-    return gIBSCBS;
-  }
+    public NFNotaInfoItemImpostoIBSCBSTIBS getGIBSCBS() {
+        return gIBSCBS;
+    }
 
-  public void setGIBSCBS(NFNotaInfoItemImpostoIBSCBSTIBS gIBSCBS) {
-    this.gIBSCBS = gIBSCBS;
-  }
+    public void setGIBSCBS(NFNotaInfoItemImpostoIBSCBSTIBS gIBSCBS) {
+        this.gIBSCBS = gIBSCBS;
+    }
 
-  public NFNotaInfoItemImpostoIBSCBSMonofasia getGIBSCBSMono() {
-    return gIBSCBSMono;
-  }
+    public NFNotaInfoItemImpostoIBSCBSMonofasia getGIBSCBSMono() {
+        return gIBSCBSMono;
+    }
 
-  public void setGIBSCBSMono(NFNotaInfoItemImpostoIBSCBSMonofasia gIBSCBSMono) {
-    this.gIBSCBSMono = gIBSCBSMono;
-  }
+    public void setGIBSCBSMono(NFNotaInfoItemImpostoIBSCBSMonofasia gIBSCBSMono) {
+        this.gIBSCBSMono = gIBSCBSMono;
+    }
 
-  public NFNotaInfoItemImpostoIBSCBSTransfCred getGTransfCred() {
-    return gTransfCred;
-  }
+    public NFNotaInfoItemImpostoIBSCBSTransfCred getGTransfCred() {
+        return gTransfCred;
+    }
 
-  public void setGTransfCred(NFNotaInfoItemImpostoIBSCBSTransfCred gTransfCred) {
-    this.gTransfCred = gTransfCred;
-  }
+    public void setGTransfCred(NFNotaInfoItemImpostoIBSCBSTransfCred gTransfCred) {
+        this.gTransfCred = gTransfCred;
+    }
 
-  public NFNotaInfoItemImpostoIBSCBSCredPresIBSZFM getGCredPresIBSZFM() {
-    return gCredPresIBSZFM;
-  }
+    public NFNotaInfoItemImpostoIBSCBSCredPresIBSZFM getGCredPresIBSZFM() {
+        return gCredPresIBSZFM;
+    }
 
-  public void setGCredPresIBSZFM(NFNotaInfoItemImpostoIBSCBSCredPresIBSZFM gCredPresIBSZFM) {
-    this.gCredPresIBSZFM = gCredPresIBSZFM;
-  }
+    public void setGCredPresIBSZFM(NFNotaInfoItemImpostoIBSCBSCredPresIBSZFM gCredPresIBSZFM) {
+        this.gCredPresIBSZFM = gCredPresIBSZFM;
+    }
 
 }
