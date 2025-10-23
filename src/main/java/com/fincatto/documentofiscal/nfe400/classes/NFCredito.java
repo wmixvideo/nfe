a30a850851e9f0290884840d254fe5023c631946
Marcos <marcoslombardi@gmail.com>
2025-10-23 09:13:42 -0300

    Cobertura testes caracteres utf-8 (#1104)
    * Ajustes NT_2025.002_v1.30_RTC_NF-e_IBS_CBS_IS e tratamento caracteres
    utf-8
    
    * Mantendo compatibilidade java 1.8
    
    * Cobertura testes
    
    * NT_2025.002_v1.30_RTC_NF-e_IBS_CBS_IS
    
    * NT_2025.002_v1.30_RTC_NF-e_IBS_CBS_IS
    
    ---------
    
    Co-authored-by: tecnosofti <tecnosofti@amdryzen>

--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/NFCredito.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/NFCredito.java
@@ -4,7 +4,7 @@ public enum NFCredito {
 
   MULTA_JUROS("01", "Multa e juros"),
   APROPRIACAO_CREDITO_PRESUMIDO_IBS_SALDO_DEVEDOR_ZFM("02", "Apropria\u00e7\u00e3o de cr\u00e9dito presumido de IBS sobre o saldo devedor na ZFM (art. 450, § 1º, LC 214/25)"),
-  RETORNO("03", "Retorno por recusa na entrega ou por n\u00e3o localiza\u00e7\u00e3o do destinat\u00e1rio na tentativa de entrega"),
+  RETORNO_RECUSA_ENTREGA_DESTINATARIO_NAO_LOCALIZADO("03", "Retorno por recusa na entrega ou por n\u00e3o localiza\u00e7\u00e3o do destinat\u00e1rio na tentativa de entrega"),
   REDUCAO_VALORES("04", "Redu\u00e7\u00e3o de valores"),
   TRANSFERENCIA_CREDITO_SUCESSAO("05", "Transfer\u00eancia de cr\u00e9dito na sucess\u00e3o");
 
