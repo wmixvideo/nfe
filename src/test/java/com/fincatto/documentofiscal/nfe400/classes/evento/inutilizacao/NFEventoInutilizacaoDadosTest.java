f3ac3ba3b61a1fcaf88676554274a490e03c31cf
fagner.couto <rogercouto01@gmail.com>
2025-10-24 09:48:42 -0400

    feat(nfe): adiciona teste do evento de inutilização

--- src/test/java/com/fincatto/documentofiscal/nfe400/classes/evento/inutilizacao/NFEventoInutilizacaoDadosTest.java
+++ src/test/java/com/fincatto/documentofiscal/nfe400/classes/evento/inutilizacao/NFEventoInutilizacaoDadosTest.java
@@ -1,31 +1,34 @@
-package com.fincatto.documentofiscal.nfe400.classes.inutilizacao;
-
-import org.junit.Assert;
-import org.junit.Test;
+package com.fincatto.documentofiscal.nfe400.classes.evento.inutilizacao;
 
 import com.fincatto.documentofiscal.DFAmbiente;
 import com.fincatto.documentofiscal.DFUnidadeFederativa;
 import com.fincatto.documentofiscal.nfe400.FabricaDeObjetosFake;
-import com.fincatto.documentofiscal.nfe400.classes.evento.inutilizacao.NFEventoInutilizacaoDados;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-public class NFEventoCancelamentoDadosTest {
+public class NFEventoInutilizacaoDadosTest {
+
+    private NFEventoInutilizacaoDados dados;
+
+    @Before
+    public void setUp() {
+        this.dados = new NFEventoInutilizacaoDados();
+    }
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirModeloInvalido() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setModeloDocumentoFiscal("75");
     }
 
     @Test
     public void devePermitirAmbosModelosDeNFe() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setModeloDocumentoFiscal("55");
         dados.setModeloDocumentoFiscal("65");
     }
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirJustificativaInvalido() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         try {
             dados.setJustificativa("rcAYGVaFoYcW8q");
         } catch (final IllegalStateException e) {
@@ -35,13 +38,11 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirCNPJInvalido() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setCnpj("1234567890123");
     }
 
     @Test(expected = NumberFormatException.class)
     public void naoDevePermitirAnoDiferenteDeDuasCasas() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         try {
             dados.setAno(9);
         } catch (final NumberFormatException e) {
@@ -51,7 +52,6 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirNumeroNFInicialInvalido() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         try {
             dados.setNumeroNFInicial("");
         } catch (final IllegalStateException e) {
@@ -61,7 +61,6 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirNumeroNFFinalInvalido() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         try {
             dados.setNumeroNFFinal("");
         } catch (final IllegalStateException e) {
@@ -71,7 +70,6 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirSerieInvalido() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         try {
             dados.setSerie("");
         } catch (final IllegalStateException e) {
@@ -81,19 +79,16 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirServicoInvalido() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setNomeServico("inutiliza");
     }
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirIDInvalido() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setIdentificador("IDw6cRIPJzP4sv6gBWQFCNcFSITQK7rOxjmBFcW2Mzf");
     }
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirIdentificadorNulo() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setAmbiente(DFAmbiente.HOMOLOGACAO);
         dados.setAno(15);
         dados.setCnpj("12345678901234");
@@ -109,7 +104,6 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirAmbienteNulo() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setAno(15);
         dados.setCnpj("12345678901234");
         dados.setIdentificador("ID55605654557305333405403926218856863798956");
@@ -125,7 +119,6 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirServicoNulo() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setAmbiente(DFAmbiente.HOMOLOGACAO);
         dados.setAno(15);
         dados.setCnpj("12345678901234");
@@ -141,7 +134,6 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirUFNulo() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setAmbiente(DFAmbiente.HOMOLOGACAO);
         dados.setAno(15);
         dados.setCnpj("12345678901234");
@@ -157,7 +149,6 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirAnoNulo() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setAmbiente(DFAmbiente.HOMOLOGACAO);
         dados.setCnpj("12345678901234");
         dados.setIdentificador("ID55605654557305333405403926218856863798956");
@@ -173,7 +164,6 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirModeloNulo() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setAmbiente(DFAmbiente.HOMOLOGACAO);
         dados.setAno(15);
         dados.setCnpj("12345678901234");
@@ -189,7 +179,6 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirSerieNulo() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setAmbiente(DFAmbiente.HOMOLOGACAO);
         dados.setAno(15);
         dados.setCnpj("12345678901234");
@@ -205,7 +194,6 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirNumeroNotaInicialNulo() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setAmbiente(DFAmbiente.HOMOLOGACAO);
         dados.setAno(15);
         dados.setCnpj("12345678901234");
@@ -221,7 +209,6 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirNumeroNotaFinalNulo() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setAmbiente(DFAmbiente.HOMOLOGACAO);
         dados.setAno(15);
         dados.setCnpj("12345678901234");
@@ -237,7 +224,6 @@ public class NFEventoCancelamentoDadosTest {
 
     @Test(expected = IllegalStateException.class)
     public void naoDevePermitirJustificativaNulo() {
-        final NFEventoInutilizacaoDados dados = new NFEventoInutilizacaoDados();
         dados.setAmbiente(DFAmbiente.HOMOLOGACAO);
         dados.setAno(15);
         dados.setCnpj("12345678901234");
@@ -255,6 +241,6 @@ public class NFEventoCancelamentoDadosTest {
     public void deveGerarXMLDeAcordoComOPadraoEstabelecido() {
         final String xmlEsperado = "<infInut Id=\"ID42161234567890123455123123456789987654321\"><tpAmb>2</tpAmb><xServ>INUTILIZAR</xServ><cUF>42</cUF><ano>16</ano><CNPJ>12345678901234</CNPJ><mod>55</mod><serie>123</serie><nNFIni>123456789</nNFIni><nNFFin>987654321</nNFFin><xJust>u2MGhwXFQDFtSuKsLkmgowBZNNhOWBL4JKIqYnIj5iDPTAUqHSwKL1O2olgmZwigRS1P58Zoc1qDxzqmvv3hBE1LYuLHNPbFXuLwM5ZxvH7xfSpnkX5VBGjrkR3cuiXLr1uz3chFb9JrNY5xU3X0eF9Byc2Q9TkPbFyPj7iRwwQVMNt6FGvpUyRMHGmhSDYhFRD2Dst0UaauvA4V0breWHyN4WUSEm9z377jXHNwtVLQQCxB2wcEIZGWVIT4CF5</xJust></infInut>";
         Assert.assertEquals(xmlEsperado, FabricaDeObjetosFake.getNFEventoInutilizacaoDados().toString());
-
     }
+
 }
