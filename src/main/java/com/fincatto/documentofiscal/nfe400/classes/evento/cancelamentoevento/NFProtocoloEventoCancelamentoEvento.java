606b8e1ad5234f9a7467bfca273a1f0e5d37b21c
Gabriel Ribeiro Bastos <tecnologia@wvetro.com.br>
2025-11-27 14:46:38 -0300

    Create NFProtocoloEventoCancelamentoEvento.java

---
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/evento/cancelamentoevento/NFProtocoloEventoCancelamentoEvento.java
@@ -0,0 +1,48 @@
+package com.fincatto.documentofiscal.nfe400.classes.evento.cancelamentoevento;
+
+import com.fincatto.documentofiscal.DFBase;
+import com.fincatto.documentofiscal.nfe400.classes.evento.NFEventoRetorno;
+import com.fincatto.documentofiscal.nfe400.classes.evento.aceitedebitoapuracao.NFEventoAceiteDebitoApuracao;
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Namespace;
+import org.simpleframework.xml.Root;
+
+@Root(name = "procEventoNFe")
+@Namespace(reference = "http://www.portalfiscal.inf.br/nfe")
+public class NFProtocoloEventoCancelamentoEvento extends DFBase {
+
+    @Attribute(name = "versao")
+    private String versao;
+
+    @Element(name = "evento")
+    private NFEventoAceiteDebitoApuracao evento;
+
+    @Element(name = "retEvento")
+    private NFEventoRetorno eventoRetorno;
+
+    public String getVersao() {
+        return versao;
+    }
+
+    public void setVersao(String versao) {
+        this.versao = versao;
+    }
+
+    public NFEventoAceiteDebitoApuracao getEvento() {
+        return evento;
+    }
+
+    public void setEvento(NFEventoAceiteDebitoApuracao evento) {
+        this.evento = evento;
+    }
+
+    public NFEventoRetorno getEventoRetorno() {
+        return eventoRetorno;
+    }
+
+    public void setEventoRetorno(NFEventoRetorno eventoRetorno) {
+        this.eventoRetorno = eventoRetorno;
+    }
+
+}
