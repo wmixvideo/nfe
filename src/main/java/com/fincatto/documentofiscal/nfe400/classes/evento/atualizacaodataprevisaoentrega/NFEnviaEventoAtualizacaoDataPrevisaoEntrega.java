5beebd20acaede86a5c5a9feac75a5f40b00223c
tecnosofti <tecnosofti@amdryzen>
2025-12-17 11:19:23 -0300

    Removendo ; duplicado e warn serial version

--- src/main/java/com/fincatto/documentofiscal/nfe400/classes/evento/atualizacaodataprevisaoentrega/NFEnviaEventoAtualizacaoDataPrevisaoEntrega.java
+++ src/main/java/com/fincatto/documentofiscal/nfe400/classes/evento/atualizacaodataprevisaoentrega/NFEnviaEventoAtualizacaoDataPrevisaoEntrega.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Namespace(reference = "http://www.portalfiscal.inf.br/nfe")
 public class NFEnviaEventoAtualizacaoDataPrevisaoEntrega extends DFBase {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 7960049528279343741L;
 
 	@Attribute(name = "versao")
     private String versao;
