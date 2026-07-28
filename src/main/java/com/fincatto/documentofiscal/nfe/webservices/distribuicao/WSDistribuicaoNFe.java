811141528ab7ec578e035bcfacf681569473c274
Marcos Lombardi de Andrade <marcoslombardi@gmail.com>
2026-07-28 11:55:14 -0300

    Add exception handling for HTTP client initialization in WSDistribuicaoNFe

--- src/main/java/com/fincatto/documentofiscal/nfe/webservices/distribuicao/WSDistribuicaoNFe.java
+++ src/main/java/com/fincatto/documentofiscal/nfe/webservices/distribuicao/WSDistribuicaoNFe.java
@@ -14,6 +14,10 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
 import java.util.Base64;
 import java.util.zip.GZIPInputStream;
 
@@ -36,7 +40,7 @@ public class WSDistribuicaoNFe {
         this.config = config;
     }
 
-    private synchronized DFHttpClient getHttpClient() throws Exception {
+    private synchronized DFHttpClient getHttpClient() throws KeyManagementException, UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
         if (this.httpClient == null) {
             final DFSocketFactory socketFactory = new DFSocketFactory(this.config);
             this.httpClient = new DFHttpClient(socketFactory.getSslContext(), this.config);
